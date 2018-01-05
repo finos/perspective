e30f0f92a3d58dd40163e5103cd1d64f3829e62d
Andrew Stein <steinlink@gmail.com>
2018-01-05 06:56:01 -0500

    Test for lazy view expansion.

--- packages/perspective-viewer-hypergrid/test/js/superstore.spec.js
+++ packages/perspective-viewer-hypergrid/test/js/superstore.spec.js
@@ -7,4 +7,40 @@
  *
  */
 
-require('@jpmorganchase/perspective-viewer/test/js/superstore.spec.js');
+const utils = require('@jpmorganchase/perspective-viewer/test/js/utils.js');
+
+const simple_tests = require('@jpmorganchase/perspective-viewer/test/js/simple_tests.js');
+
+async function set_lazy(page) {
+	const viewer = await page.$('perspective-viewer');
+    await page.evaluate(element => { 
+    	element.grid.grid.properties.repaintIntervalRate = 1;
+    	Object.defineProperty(element.grid.grid, '_lazy_load', {
+    		set: () => {},
+    		get: () => true
+    	});
+    }, viewer);
+}
+
+utils.with_server({}, () => {
+
+    describe.page("superstore.html", () => {
+
+        simple_tests.default();
+
+        describe("lazy render mode", () => {
+
+		    test.capture("resets viewable area when the logical size expands.", async page => {        
+		        await set_lazy(page);
+		        await page.click('#config_button');
+		        const viewer = await page.$('perspective-viewer');
+		        await page.evaluate(element => element.setAttribute('column-pivots', '["Category"]'), viewer);
+		        await page.waitForSelector('perspective-viewer:not([updating])');
+				await page.evaluate(element => element.setAttribute('row-pivots', '["City"]'), viewer);
+		    });
+
+		});
+
+    });
+
+});
