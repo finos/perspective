3f0ee17d71551020d3b69514d7b8f3792816077c
Tim Paine <t.paine154@gmail.com>
2018-01-19 19:26:48 -0500

    adding jupyterlab extension

---
+++ jslab/src/index.ts
@@ -0,0 +1,108 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+import {
+  Widget
+} from '@phosphor/widgets';
+
+import {
+  Message
+} from '@phosphor/messaging';
+
+import {
+  IRenderMime
+} from '@jupyterlab/rendermime-interfaces';
+
+import '../style/index.css';
+
+import "@jpmorganchase/perspective-viewer";
+import "@jpmorganchase/perspective-viewer-hypergrid";
+import "@jpmorganchase/perspective-viewer-highcharts";
+
+export
+const MIME_TYPE = 'application/psp';
+
+export
+const PSP_CLASS = 'jp-PSPViewer';
+
+export
+const PSP_CONTAINER_CLASS = 'jp-PSPContainer';
+
+export
+class RenderedPSP extends Widget implements IRenderMime.IRenderer {
+  constructor() {
+    super({ node: Private.createNode() });
+  }
+
+  onAfterAttach(msg: Message) : void{
+      if (this._loaded) return;
+      let x = (<any>(this.node.querySelector('perspective-viewer')));
+      x.load(this.data);
+      this._loaded = true;
+  }
+
+  renderModel(model: IRenderMime.IMimeModel): Promise<void> {
+    console.log('\n\n\n\n\n\nFUUUUUUCK\n\n\n\n\n');
+    let data2 = [   
+        {'x': 1, 'y':'a', 'z': true},
+        {'x': 2, 'y':'b', 'z': false},
+        {'x': 3, 'y':'c', 'z': true},
+        {'x': 4, 'y':'d', 'z': false}
+    ];
+    this.data = data2;
+    return Promise.resolve();
+  }
+
+  data: object;
+  private _loaded: boolean;
+}
+
+
+export
+const rendererFactory: IRenderMime.IRendererFactory = {
+  safe: false,
+  mimeTypes: [MIME_TYPE],
+  createRenderer: options => new RenderedPSP()
+};
+
+
+const extensions: IRenderMime.IExtension | IRenderMime.IExtension[] = [
+  {
+    id: 'perspective:factory',
+    rendererFactory,
+    dataType: 'string',
+    fileTypes: [{
+      name: 'perspective',
+      fileFormat: 'base64',
+      mimeTypes: [MIME_TYPE],
+      extensions: ['psp']
+    }],
+    documentWidgetFactoryOptions: {
+      name: 'perspective',
+      modelName: 'base64',
+      primaryFileType: 'psp',
+      fileTypes: ['psp'],
+      defaultFor: ['psp']
+    }
+  }
+];
+
+export default extensions;
+
+
+namespace Private {
+  export
+  let _loaded = false;
+
+  export
+  function createNode(): HTMLElement {
+    let node = document.createElement('div');
+    node.className = PSP_CONTAINER_CLASS;
+    let psp = document.createElement('perspective-viewer');
+    psp.className = PSP_CLASS;
+    psp.setAttribute('type', MIME_TYPE);
+    node.appendChild(psp);
+    return node;
+  }
+
+}
+
