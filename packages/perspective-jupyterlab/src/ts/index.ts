992910e181fc01b0b0382ae3b3c03d4c27f3ab76
Tim Paine <t.paine154@gmail.com>
2018-01-31 10:19:01 -0500

    prepping for merge

--- packages/perspective-jupyterlab/src/ts/index.ts
+++ packages/perspective-jupyterlab/src/ts/index.ts
@@ -1,5 +1,12 @@
-// Copyright (c) Jupyter Development Team.
-// Distributed under the terms of the Modified BSD License.
+/******************************************************************************
+ *
+ * Copyright (c) 2017, the Perspective Authors.
+ *
+ * This file is part of the Perspective library, distributed under the terms of
+ * the Apache License 2.0.  The full license can be found in the LICENSE file.
+ *
+ */
+
 import {
   Widget
 } from '@phosphor/widgets';
