288cf431b175d9d4b5838d7456f1c0df4b642fac
Andrew Stein <steinlink@gmail.com>
2018-02-13 04:56:55 -0500

    Fixed bubble chart regression, reordered imports to properly capture prototypes.

--- packages/perspective-viewer-highcharts/src/js/heatmap.js
+++ packages/perspective-viewer-highcharts/src/js/heatmap.js
@@ -8,14 +8,12 @@
  */
 
 import highcharts from 'highcharts';
+import highchartsMore from 'highcharts-more';
 import heatmap from 'highcharts/modules/heatmap';
 import grouped_categories from 'highcharts-grouped-categories';
 import chroma from 'chroma-js';
 
 const Highcharts = highcharts;
-
-heatmap(highcharts);
-grouped_categories(highcharts);
 
 // cache prototypes
 let axisProto = Highcharts.Axis.prototype,
@@ -26,6 +24,9 @@ let axisProto = Highcharts.Axis.prototype,
     protoAxisRender = axisProto.render,
     UNDEFINED = void 0;
 
+highchartsMore(highcharts);
+heatmap(highcharts);
+grouped_categories(highcharts);
 
 export const COLORS_10 = ['#1f77b4','#ff7f0e','#2ca02c','#d62728','#9467bd','#8c564b','#e377c2','#7f7f7f','#bcbd22','#17becf'];
 export const COLORS_20 = [
