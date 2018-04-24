82b08ccf9956dfe79898b6f5acbb13e83be21bd5
Andrew Stein <steinlink@gmail.com>
2018-04-24 00:40:06 -0400

    Fixed -examples modules

--- packages/perspective/src/config/perspective.node.config.js
+++ packages/perspective/src/config/perspective.node.config.js
@@ -4,6 +4,7 @@ const common = require('./common.config.js');
 module.exports = Object.assign({}, common(), {
     entry: './src/js/perspective.node.js',
     target: "node",
+    externals: [/^[a-z0-9\@].*$/],
     plugins: [],
     node: {},
     output: {
