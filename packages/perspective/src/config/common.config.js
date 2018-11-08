0509e71c09363033336cbd50fa62102b61c4586e
Andrew Stein <steinlink@gmail.com>
2018-11-07 23:07:28 -0500

    Fixed benchmark build

--- packages/perspective/src/config/common.config.js
+++ packages/perspective/src/config/common.config.js
@@ -4,20 +4,21 @@ const PerspectivePlugin = require("../../webpack-plugin");
 
 const plugins = [new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /(en|es|fr)$/)];
 
-if (!process.env.PSP_NO_MINIFY && !process.env.PSP_DEBUG) {
-    plugins.push(
-        new UglifyJSPlugin({
-            sourceMap: true,
-            mangle: false,
-            exclude: /asmjs\.worker\.js$/,
-            output: {
-                ascii_only: true
-            }
-        })
-    );
-}
+module.exports = function({build_worker, no_minify} = {}) {
+    
+    if (!process.env.PSP_NO_MINIFY && !process.env.PSP_DEBUG && !no_minify) {
+        plugins.push(
+            new UglifyJSPlugin({
+                sourceMap: true,
+                mangle: false,
+                exclude: /(asmjs\.worker\.js)$/,
+                output: {
+                    ascii_only: true
+                }
+            })
+        );
+    }
 
-module.exports = function(build_worker) {
     plugins.push(new PerspectivePlugin({build_worker: build_worker}));
     return {
         plugins: plugins,
