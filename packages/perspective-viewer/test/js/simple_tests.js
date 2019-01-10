67131a5a9a01ff38043fc88b890571fffe80dad2
Ro4052 <Ro4052@users.noreply.github.com>
2019-01-10 14:52:12 +0000

    Added screenshot test for invalid filter highlighting

--- packages/perspective-viewer/test/js/simple_tests.js
+++ packages/perspective-viewer/test/js/simple_tests.js
@@ -65,6 +65,12 @@ exports.default = function() {
         await page.evaluate(element => element.setAttribute("filters", '[["Sales", ">", 500]]'), viewer);
     });
 
+    test.capture("highlights invalid filter.", async page => {
+        const viewer = await page.$("perspective-viewer");
+        await page.shadow_click("perspective-viewer", "#config_button");
+        await page.evaluate(element => element.setAttribute("filters", '[["Sales", "==", null]]'), viewer);
+    });
+
     test.capture("sorts by an alpha column.", async page => {
         const viewer = await page.$("perspective-viewer");
         await page.shadow_click("perspective-viewer", "#config_button");
