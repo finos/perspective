0c1d56ce9fdb9106fac49c31de187e0e6d25b982
Ro4052 <Ro4052@users.noreply.github.com>
2019-01-17 13:57:57 +0000

    Bar positions are now adjusted when visibility is toggled with the key

--- packages/perspective-viewer-d3fc/src/js/d3fcChart.js
+++ packages/perspective-viewer-d3fc/src/js/d3fcChart.js
@@ -16,35 +16,38 @@ export default class D3FCChart {
     this._mode = mode;
     this._config = config;
     this._container = container;
+    this._hiddenElements = [];
+    this.update = this.update.bind(this);
   }
 
   render() {
     console.log("config:", this._config);
 
     if (this._mode === "x_bar") {
-      renderBar(this._config, this._container, true);
+      renderBar(this._config, this._container, true, this._hiddenElements, this.update);
     } else if (this._mode === "y_bar") {
-      renderBar(this._config, this._container, false);
+      renderBar(this._config, this._container, false, this._hiddenElements, this.update);
     } else {
       throw "EXCEPTION: chart type not recognised.";
     }
   }
 
   update() {
+    d3.select(this._container).selectAll("*").remove();
     this.render();
   }
 
 }
 
-function renderBar(config, container, horizontal) {
+function renderBar(config, container, horizontal, hiddenElements, update) {
   let orientation = horizontal ? "horizontal" : "vertical";
 
   let labels = interpretLabels(config);
   let isSplitBy = labels.splitLabel != null;
 
-  let [dataset, stackedBarData, color] = interpretDataset(isSplitBy, config);
+  let [dataset, stackedBarData, color] = interpretDataset(isSplitBy, config, hiddenElements);
 
-  let legend = configureLegend(isSplitBy, color, container);
+  let legend = configureLegend(isSplitBy, color, hiddenElements, update);
   let barSeries = configureBarSeries(isSplitBy, orientation, dataset);
   let gridlines = configureGrid(horizontal);
   let [xScale, yScale] = configureScale(isSplitBy, horizontal, dataset, stackedBarData);
@@ -64,7 +67,7 @@ function renderBar(config, container, horizontal) {
     .datum(dataset)
     .call(chart);
 
-  drawLegend(legend, container);
+  drawLegend(legend, container, hiddenElements);
 }
 
 // CONFIGURE CHART ELEMENTS
@@ -149,12 +152,12 @@ function configureMultiSvg(isSplitBy, gridlines, barSeries, dataset, color) {
   let multi;
   if (isSplitBy) {
     let multiWithOutGrid = fc.seriesSvgMulti()
-      .mapping((data, index, nodes) => data[index])
+      .mapping((data, index) => data[index])
       .series(barSeries)
       .decorate((selection) => {
         selection
-          .each((data, index, nodes) => {
-            d3.select(nodes[index])
+          .each(function(data, index) {
+            d3.select(this)
               .selectAll('g.bar')
               .attr('fill', color(dataset[index].key));
           });
@@ -179,7 +182,7 @@ function configureMultiSvg(isSplitBy, gridlines, barSeries, dataset, color) {
   return multi;
 }
 
-function configureLegend(isSplitBy, color, container) {
+function configureLegend(isSplitBy, color, hiddenElements, update) {
   if (!isSplitBy) {
     return;
   }
@@ -191,31 +194,25 @@ function configureLegend(isSplitBy, color, container) {
     .orient('vertical')
     .scale(color)
     .on('cellclick', function (d) {
-      toggleBars(d);
-      const legendCell = d3.select(this);
-      legendCell.classed('hidden', !legendCell.classed('hidden'));
+      const index = hiddenElements.findIndex(e => e === d);
+      index >= 0 ? hiddenElements.splice(index, 1) : hiddenElements.push(d);
+      update();
     });
-
-  function toggleBars(colorClass) {
-    console.log(colorClass);
-    d3.select(container).selectAll(`g.${colorClass}`)
-      .classed('hidden', function () {  // toggle "hidden" class
-        return !d3.select(this).classed('hidden');
-      });
-  }
 
   return legend;
 }
 
 
 // DRAW CHART ELEMENTS
-function drawLegend(legend, container) {
+function drawLegend(legend, container, hiddenElements) {
   if (legend) {
     d3.select(container)
       .append("svg")
       .attr("class", "legend")
       .style("z-index", "2")
-      .call(legend);
+      .call(legend)
+      .selectAll("g.cell")
+      .classed("hidden", data => hiddenElements.includes(data));
   }
 }
 
@@ -237,9 +234,9 @@ function interpretLabels(config) {
   return labels;
 }
 
-function interpretDataset(isSplitBy, config) {
+function interpretDataset(isSplitBy, config, hiddenElements) {
   if (isSplitBy) {
-    let [dataset, stackedBarData, color] = interpretStackDataset(config);
+    let [dataset, stackedBarData, color] = interpretStackDataset(config, hiddenElements);
     console.log("dataset: ", dataset);
     return [dataset, stackedBarData, color];
   }
@@ -259,13 +256,13 @@ function interpretDataset(isSplitBy, config) {
   return [dataset, null, null];
 }
 
-function interpretStackDataset(config) {
+function interpretStackDataset(config, hiddenElements) {
   //Convert data to Stacked Bar Chart Format
   let keys = config.xAxis.categories.length > 0 ? config.xAxis.categories : [...Array(config.series[0].data.length)].map((_, i) => i)
 
   let stackedBarData = keys.map((group, i) => {
     let row = { group }
-    config.series.forEach(split => {
+    config.series.filter(d => !hiddenElements.includes(d.name)).forEach(split => {
       row[split.name] = split.data[i] || 0;
     });
     return row;
@@ -273,7 +270,7 @@ function interpretStackDataset(config) {
 
   let stack = d3.stack().keys(Object.keys(stackedBarData[0]).filter(r => r !== "group"));
   let dataset = stack(stackedBarData);
-  let color = d3.scaleOrdinal(d3.schemeCategory10).domain(dataset.map(s => s.key));
+  let color = d3.scaleOrdinal(d3.schemeCategory10).domain(config.series.map(s => s.name));
   return [dataset, stackedBarData, color];
 }
 
