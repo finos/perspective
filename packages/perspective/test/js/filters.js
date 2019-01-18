9548546007979394f64c7d1a26750500c60f658f
Ro4052 <Ro4052@users.noreply.github.com>
2019-01-18 11:51:54 +0000

    Updated filters tests for new empty filter behaviour

--- packages/perspective/test/js/filters.js
+++ packages/perspective/test/js/filters.js
@@ -280,20 +280,6 @@ module.exports = perspective => {
                 view.delete();
                 table.delete();
             });
-
-            it('x == ""', async function() {
-                var table = perspective.table({x: "float", y: "integer"});
-                const dataSet = [{x: 3.5, y: 1}, {x: 2.5, y: 1}, {x: null, y: 1}, {x: null, y: 1}, {x: 4.5, y: 2}, {x: null, y: 2}];
-                table.update(dataSet);
-                var view = table.view({
-                    filter: [["x", "==", ""]]
-                });
-                var answer = dataSet;
-                let result = await view.to_json();
-                expect(answer).toEqual(result);
-                view.delete();
-                table.delete();
-            });
         });
 
         describe("is_valid_filter", function() {
@@ -318,7 +304,7 @@ module.exports = perspective => {
             it('x == ""', async function() {
                 var table = perspective.table(data);
                 let isValid = await table.is_valid_filter(["x", "==", ""]);
-                expect(isValid).toBeFalsy();
+                expect(isValid).toBeTruthy();
                 table.delete();
             });
             it("valid date", async function() {
