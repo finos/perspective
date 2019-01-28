9ba48c3fa78cb4f395e18183ec80833d92a020e3
Matthew Hooper <mhooper@scottlogic.co.uk>
2019-01-28 09:50:17 +0000

    Add interpretKeysAndColor

--- packages/perspective-viewer-d3fc/src/js/d3fcChart.js
+++ packages/perspective-viewer-d3fc/src/js/d3fcChart.js
@@ -8,8 +8,8 @@
  */
 
 import * as d3 from "d3";
-import {configureLegend, configureBarSeries, configureGrid, configureScale, configureMultiSvg, configureChart, configureMultiColumnBarSeries, configureScaleMultiColumn, configureMultiSeries} from "./chartConfig";
-import {interpretLabels, interpretGroupBys, interpretDataset, interpretKeysAndColor, interpretMultiColumnDataset, interpretIsMultiColumn} from "./dataInterpretation";
+import {configureLegend, configureBarSeries, configureGrid, configureScale, configureMultiSvg, configureChart} from "./chartConfig";
+import {interpretLabels, interpretGroupBys, interpretDataset, interpretKeysAndColor} from "./dataInterpretation";
 
 const nbsp = "\xa0";
 
@@ -52,29 +52,20 @@ export default class D3FCChart {
 
         let labels = interpretLabels(config);
         let isSplitBy = labels.splitLabel.length !== 0;
-        const isMultiColumn = interpretIsMultiColumn(config);
 
+        let groupBys = interpretGroupBys(config.xAxis.categories);
         let series = config.series;
-        let groupBys = interpretGroupBys(config.xAxis.categories, series);
 
         const [keys, color] = interpretKeysAndColor(config);
         let [dataset, stackedBarData] = interpretDataset(isSplitBy, series, groupBys, hiddenElements);
 
-        let legend = configureLegend(config.legend.enabled, color, hiddenElements, update);
+        let legend = configureLegend(isSplitBy, color, hiddenElements, update);
         let barSeries = configureBarSeries(isSplitBy, orientation, dataset);
         let gridlines = configureGrid(horizontal);
         let [xScale, yScale] = configureScale(isSplitBy, horizontal, dataset, stackedBarData);
+
         // groups of svgs we need to render
         let multi = configureMultiSvg(isSplitBy, gridlines, barSeries, dataset, color);
-        let groupedBarData;
-
-        if (isMultiColumn) {
-            [dataset, groupedBarData] = interpretMultiColumnDataset(config, hiddenElements);
-            barSeries = configureMultiColumnBarSeries(orientation, color, keys);
-            [xScale, yScale] = configureScaleMultiColumn(horizontal, dataset, groupedBarData);
-            multi = configureMultiSeries(gridlines, barSeries);
-        }
-
         let chart = configureChart(xScale, yScale, multi);
 
         styleChart(chart, horizontal, labels);
