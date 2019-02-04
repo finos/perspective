1fafe1291ccf7c57ac5c55d23d9ac1afc0b7c56a
Luke Sheard <me@lukesheard.com>
2019-02-04 16:11:55 +0000

    fix negation in cross origin function

--- examples/webpack/webpack.config.js
+++ examples/webpack/webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
             },
             workerLoaderOptions: {
                 name: "[hash].worker.[ext]"
-            },
+            }
         })
     ],
     devtool: "source-map",
