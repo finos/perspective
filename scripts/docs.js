9a3efaeabcba794d039351001c5082f3896c67f1
Tim Paine <t.paine154@gmail.com>
2019-02-06 14:22:30 -0500

    working for javascript, set up dockerimage

--- scripts/docs.js
+++ scripts/docs.js
@@ -10,12 +10,28 @@
 const execSync = require("child_process").execSync;
 const execute = cmd => execSync(cmd, {stdio: "inherit"});
 
+function docker(image = "emsdk") {
+    console.log(`-- Creating ${image} docker image`);
+    let cmd = "docker run --rm -it";
+    if (process.env.PSP_CPU_COUNT) {
+        cmd += ` --cpus="${parseInt(process.env.PSP_CPU_COUNT)}.0"`;
+    }
+    cmd += ` -v $(pwd):/usr/src/app -w /usr/src/app/ perspective/${image}`;
+    return cmd;
+}
+
 let flags = " -DPSP_WASM_BUILD=0 -DPSP_CPP_BUILD=0 -DPSP_PYTHON_BUILD=1 -DPSP_BUILD_DOCS=1";
 
 try {
-    execute("mkdir -p docsbuild");
     let cmd = "cd docsbuild && cmake ..  -DPSP_WASM_BUILD=0 -DPSP_CPP_BUILD=0 -DPSP_PYTHON_BUILD=0 -DPSP_BUILD_DOCS=1 && make -j${PSP_CPU_COUNT-8}";
-    execute(cmd);
+    if (process.env.PSP_DOCKER) {
+        execute(docker("docs") + " mkdir -p docsbuild");
+        execute(cmd);
+
+    } else {
+        execute("mkdir -p docsbuild");
+        execute(cmd);
+    }
 } catch (e) {
     console.log(e.message);
     process.exit(1);
