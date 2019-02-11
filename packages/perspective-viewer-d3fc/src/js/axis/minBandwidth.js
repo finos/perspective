4194561d816df765853fad31acd7421b51055e1d
Andy <alee@scottlogic.co.uk>
2019-02-11 15:32:51 +0000

    Minimum 1-pixel bandwidth
    Avoids lines disappearing because they are too narrow (only works for scaleBand).
    Also improved implementation of `withoutTicks` band

---
+++ packages/perspective-viewer-d3fc/src/js/axis/minBandwidth.js
@@ -0,0 +1,29 @@
+/******************************************************************************
+ *
+ * Copyright (c) 2017, the Perspective Authors.
+ *
+ * This file is part of the Perspective library, distributed under the terms of
+ * the Apache License 2.0.  The full license can be found in the LICENSE file.
+ *
+ */
+import {rebindAll} from "d3fc";
+
+const MIN_BANDWIDTH = 1;
+
+export default adaptee => {
+    const minBandwidth = arg => {
+        return adaptee(arg);
+    };
+
+    rebindAll(minBandwidth, adaptee);
+
+    minBandwidth.bandwidth = (...args) => {
+        if (!args.length) {
+            return Math.max(adaptee.bandwidth(), MIN_BANDWIDTH);
+        }
+        adaptee.bandwidth(...args);
+        return minBandwidth;
+    };
+
+    return minBandwidth;
+};
---
+++ packages/perspective-viewer-d3fc/src/js/axis/withoutTicks.js
@@ -0,0 +1,23 @@
+/******************************************************************************
+ *
+ * Copyright (c) 2017, the Perspective Authors.
+ *
+ * This file is part of the Perspective library, distributed under the terms of
+ * the Apache License 2.0.  The full license can be found in the LICENSE file.
+ *
+ */
+import {rebindAll} from "d3fc";
+
+export default adaptee => {
+    const withoutTicks = arg => {
+        return adaptee(arg);
+    };
+
+    rebindAll(withoutTicks, adaptee);
+
+    withoutTicks.ticks = function() {
+        return [];
+    };
+
+    return withoutTicks;
+};
