da12fec257fd366d0032ac952685dde79a7f7f46
Ro4052 <Ro4052@users.noreply.github.com>
2019-02-13 12:01:26 +0000

    Switched from html string to template

--- packages/perspective-viewer-d3fc/src/js/tooltip/tooltip.js
+++ packages/perspective-viewer-d3fc/src/js/tooltip/tooltip.js
@@ -1,18 +1,17 @@
 import {select} from "d3";
 import {getChartElement} from "../plugin/root";
+import tooltipTemplate from "../../html/tooltip.html";
 
 export function tooltip(selection, settings) {
+    const container = select(getChartElement(selection.node()).getContainer());
+    const tooltipDiv = getTooltipDiv(container);
     selection
         .filter(d => d.baseValue !== d.mainValue)
         .on("mouseover", function(data) {
-            const container = select(getChartElement(this).getContainer());
-            const tooltipDiv = getTooltipDiv(container);
-            const html = generateHtml(data, settings);
-            showTooltip(container.node(), this, tooltipDiv, html);
+            generateHtml(tooltipDiv, data, settings);
+            showTooltip(container.node(), this, tooltipDiv);
         })
         .on("mouseout", function() {
-            const container = select(getChartElement(this).getContainer());
-            const tooltipDiv = getTooltipDiv(container);
             hideTooltip(tooltipDiv);
         });
 }
@@ -24,33 +23,41 @@ function getTooltipDiv(container) {
             .append("div")
             .attr("class", "tooltip")
             .style("z-index", 3)
-            .style("opacity", 0);
+            .style("opacity", 0)
+            .html(tooltipTemplate);
     }
     return tooltipDiv;
 }
 
-function generateHtml(data, settings) {
+function generateHtml(tooltipDiv, data, settings) {
     const splits = data.key.split("|");
-    let html = [];
 
     // Add group data
     if (settings.crossValues.length) {
         const groups = data.crossValue.split(",");
-        html = html.concat(settings.crossValues.map((group, i) => `${group.name}: <b>${groups[i]}</b>`));
+        tooltipDiv
+            .select("#cross-values")
+            .selectAll("li")
+            .data(groups)
+            .join("li")
+            .text((d, i) => `${settings.crossValues[i].name}: ${d}`);
     }
 
     // Add split data
     if (settings.splitValues.length) {
-        html = html.concat(settings.splitValues.map((split, i) => `${split.name}: <b>${splits[i]}</b>`));
+        tooltipDiv
+            .select("#split-values")
+            .selectAll("li")
+            .data(splits.slice(0, -1))
+            .join("li")
+            .text((d, i) => `${settings.splitValues[i].name}: ${d}`);
     }
 
-    // Add value
-    html.push(`${splits[splits.length - 1]}: <b>${data.mainValue - data.baseValue}</b>`);
-
-    return html.join("</br>");
+    // Add data value
+    tooltipDiv.select("#data-value").text(`${splits[splits.length - 1]}: ${data.mainValue - data.baseValue}`);
 }
 
-function showTooltip(containerNode, barNode, tooltipDiv, html) {
+function showTooltip(containerNode, barNode, tooltipDiv) {
     const containerRect = containerNode.getBoundingClientRect();
     const barRect = barNode.getBoundingClientRect();
     const left = barRect.left + barRect.width / 2 - containerRect.left;
@@ -58,7 +65,6 @@ function showTooltip(containerNode, barNode, tooltipDiv, html) {
     tooltipDiv
         .style("left", `${left}px`)
         .style("top", `${top}px`)
-        .html(html)
         .transition()
         .duration(200)
         .style("opacity", 0.9);
