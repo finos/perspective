d73ad04e59044e0f54203edb4b060f118a4c89af
Ro4052 <Ro4052@users.noreply.github.com>
2019-02-14 10:16:33 +0000

    Refactored getOrCreateElement function

--- packages/perspective-viewer-d3fc/src/js/utils/utils.js
+++ packages/perspective-viewer-d3fc/src/js/utils/utils.js
@@ -4,6 +4,5 @@ export function areArraysEqualSimple(arr1, arr2) {
 
 export function getOrCreateElement(container, selector, createCallback) {
     let element = container.select(selector);
-    if (element.size() === 0) element = createCallback();
-    return element;
+    return element.size() > 0 ? element : createCallback();
 }
