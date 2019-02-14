86fad80eef763e0f921d1fba60d15f9b6fba471c
Andy <alee@scottlogic.co.uk>
2019-02-14 14:27:26 +0000

    Moved scrollable-legend into a standalone component
    Separated the scrollable code from the perspective-d3fc code to create a standalone "scrollable" legend component

--- packages/perspective-viewer-d3fc/src/js/legend/legend.js
+++ packages/perspective-viewer-d3fc/src/js/legend/legend.js
@@ -6,117 +6,51 @@
  * the Apache License 2.0.  The full license can be found in the LICENSE file.
  *
  */
-import * as d3Legend from "d3-svg-legend";
-import {getChartElement} from "../plugin/root";
-import {getOrCreateElement} from "../utils/utils";
-import legendControlsTemplate from "../../html/legend-controls.html";
 
+import scrollableLegend from "./scrollableLegend";
+import {getChartElement} from "../plugin/root";
+
+const scrollLegend = scrollableLegend();
 export function legend(container, settings, colour) {
     if (colour) {
-        const groupSize = 15;
-        const numberOfGroups = Math.ceil(colour.domain().length / groupSize);
-        const doesLegendScroll = numberOfGroups > 1;
+        scrollLegend
+            .scale(colour)
+            .shape("circle")
+            .shapeRadius(6)
+            .orient("vertical")
+            .on("cellclick", function(d) {
+                settings.hideKeys = settings.hideKeys || [];
+                if (settings.hideKeys.includes(d)) {
+                    settings.hideKeys = settings.hideKeys.filter(k => k !== d);
+                } else {
+                    settings.hideKeys.push(d);
+                }
 
-        if (doesLegendScroll) {
-            const reloadLegend = index => createLegend(container, settings, colour, doesLegendScroll, groupSize, index);
-            legendScrolling(container, settings, numberOfGroups, reloadLegend);
+                getChartElement(this).draw();
+            });
+
+        if (settings.mainValues.length <= 1) {
+            scrollLegend.labels(options => {
+                const parts = options.domain[options.i].split("|");
+                return parts.slice(0, parts.length - 1).join("|");
+            });
         }
-        createLegend(container, settings, colour, doesLegendScroll, groupSize, settings.legendGroupIndex);
-    }
-}
 
-function legendScrolling(container, settings, numberOfGroups, reloadLegend) {
-    if (!settings.legendGroupIndex) settings.legendGroupIndex = 0;
-    const legendControls = getLegendControls(container);
-
-    const setPageText = () => legendControls.select("#page-text").text(`${settings.legendGroupIndex + 1}/${numberOfGroups}`);
-    setPageText();
-    const upArrow = legendControls.select("#up-arrow");
-    const downArrow = legendControls.select("#down-arrow");
-
-    upArrow.on("click", function() {
-        if (settings.legendGroupIndex > 0) {
-            settings.legendGroupIndex--;
-            legendPage(downArrow);
+        let legendSelection = container.select("div.legend-container");
+        if (legendSelection.size() === 0) {
+            legendSelection = container.append("div");
         }
-        deactivateArrow(upArrow, 0);
-    });
-    deactivateArrow(upArrow, 0);
 
-    downArrow.on("click", function() {
-        if (settings.legendGroupIndex < numberOfGroups - 1) {
-            settings.legendGroupIndex++;
-            legendPage(upArrow);
-        }
-        deactivateArrow(downArrow, numberOfGroups - 1);
-    });
-    if (settings.legendGroupIndex < numberOfGroups - 1) activateArrow(downArrow);
-
-    function legendPage(arrow) {
-        reloadLegend(settings.legendGroupIndex);
-        setPageText();
-        activateArrow(arrow);
+        // render the legend
+        legendSelection
+            .attr("class", "legend-container")
+            .style("z-index", "2")
+            .call(scrollLegend)
+            .select("g.legendCells")
+            .attr("transform", "translate(20,20)")
+            .selectAll("g.cell")
+            .classed("hidden", data => settings.hideKeys && settings.hideKeys.includes(data));
     }
-
-    function activateArrow(arrow) {
-        arrow.style("color", "rgb(63, 127, 253)").style("cursor", "pointer");
-    }
-
-    function deactivateArrow(arrow, threshold) {
-        if (settings.legendGroupIndex === threshold) arrow.style("color", null).style("cursor", "default");
-    }
-}
-
-function getLegendControls(container) {
-    return getOrCreateElement(container, ".legend-controls", () =>
-        container
-            .append("div")
-            .attr("class", "legend-controls")
-            .html(legendControlsTemplate)
-    );
-}
-
-function createLegend(container, settings, colour, doesLegendScroll, groupSize, groupIndex) {
-    var legend = d3Legend
-        .legendColor()
-        .scale(colour)
-        .shape("circle")
-        .shapeRadius(6)
-        .orient("vertical")
-        .on("cellclick", function(d) {
-            settings.hideKeys = settings.hideKeys || [];
-            if (settings.hideKeys.includes(d)) {
-                settings.hideKeys = settings.hideKeys.filter(k => k !== d);
-            } else {
-                settings.hideKeys.push(d);
-            }
-
-            getChartElement(this).draw();
-        });
-    if (doesLegendScroll) legend.cellFilter(cellFilter(groupSize, groupIndex));
-
-    if (settings.mainValues.length <= 1) {
-        legend.labels(options => {
-            const parts = options.domain[options.i].split("|");
-            return parts.slice(0, -1).join("|");
-        });
-    }
-
-    const legendSelection = getOrCreateElement(container, "svg.legend", () => container.append("svg"));
-
-    // render the legend
-    legendSelection
-        .attr("class", "legend")
-        .style("z-index", "2")
-        .call(legend)
-        .select("g.legendCells")
-        .attr("transform", "translate(20,20)")
-        .selectAll("g.cell")
-        .classed("hidden", data => settings.hideKeys && settings.hideKeys.includes(data));
-}
-
-function cellFilter(groupSize, groupIndex) {
-    return (_, i) => i >= groupSize * groupIndex && i < groupSize * groupIndex + groupSize;
 }
 
 export function filterData(settings, data) {
