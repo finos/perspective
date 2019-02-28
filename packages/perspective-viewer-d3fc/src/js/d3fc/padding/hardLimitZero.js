39dca1dac06572992903250bdfa768d7aab485be
Sam Sutherland Grant <redbear.sam@hotmail.com>
2019-02-28 11:27:41 +0000

    remove function and property that allows a padding strat's purpose to be fundamentally undermined

--- packages/perspective-viewer-d3fc/src/js/d3fc/padding/hardLimitZero.js
+++ packages/perspective-viewer-d3fc/src/js/d3fc/padding/hardLimitZero.js
@@ -11,7 +11,6 @@ import {defaultPadding} from "./default";
 import * as fc from "d3fc";
 
 export const hardLimitZeroPadding = () => {
-    let padAcrossZero = false;
     const _defaultPadding = defaultPadding();
 
     const padding = extent => {
@@ -34,21 +33,13 @@ export const hardLimitZeroPadding = () => {
         let paddedLowerExtent = extent[0] - pad[0] * delta;
         let paddedUpperExtent = extent[1] + pad[1] * delta;
 
-        // If datapoints are exclusively negative or exclusively positive and padAcrossZero is set false, hard limit extent to 0.
-        extent[0] = !padAcrossZero && extent[0] >= 0 && paddedLowerExtent < 0 ? 0 : paddedLowerExtent;
-        extent[1] = !padAcrossZero && extent[1] <= 0 && paddedUpperExtent > 0 ? 0 : paddedUpperExtent;
+        // If datapoints are exclusively negative or exclusively positive hard limit extent to 0.
+        extent[0] = extent[0] >= 0 && paddedLowerExtent < 0 ? 0 : paddedLowerExtent;
+        extent[1] = extent[1] <= 0 && paddedUpperExtent > 0 ? 0 : paddedUpperExtent;
         return extent;
     };
 
     fc.rebindAll(padding, _defaultPadding);
-
-    padding.padAcrossZero = function() {
-        if (!arguments.length) {
-            return padAcrossZero;
-        }
-        padAcrossZero = arguments.length <= 0 ? undefined : arguments[0];
-        return padding;
-    };
 
     return padding;
 };
