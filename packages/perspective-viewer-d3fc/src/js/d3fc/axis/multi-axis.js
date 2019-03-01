d1fddae31224beaefb282afcf7b1ceccded2c3e7
Andy <alee@scottlogic.co.uk>
2019-03-01 15:11:11 +0000

    Support different sizes for the different levels of the axis labels

--- packages/perspective-viewer-d3fc/src/js/d3fc/axis/multi-axis.js
+++ packages/perspective-viewer-d3fc/src/js/d3fc/axis/multi-axis.js
@@ -4,7 +4,9 @@ import {axisTop, axisBottom, axisLeft, axisRight} from "./axis";
 import store from "../chart/store";
 
 const multiAxis = (orient, baseAxis, scale) => {
-    let axisStore = store("tickFormat", "ticks", "tickArguments", "tickSize", "tickSizeInner", "tickSizeOuter", "tickValues", "tickPadding", "centerAlignTicks");
+    let tickSizeOuter = 6;
+    let tickSizeInner = 6;
+    let axisStore = store("tickFormat", "ticks", "tickArguments", "tickValues", "tickPadding", "centerAlignTicks");
     let decorate = () => {};
 
     let groups = null;
@@ -38,7 +40,6 @@ const multiAxis = (orient, baseAxis, scale) => {
 
             // add the domain line
             const range = scale.range();
-            const tickSizeOuter = groups.length * multiAxis.tickSizeOuter();
             const domainPathData = pathTranspose([[range[0], sign * tickSizeOuter], [range[0], 0], [range[1], 0], [range[1], sign * tickSizeOuter]]);
 
             const domainLine = domainPathDataJoin(container, [data]);
@@ -49,23 +50,32 @@ const multiAxis = (orient, baseAxis, scale) => {
 
             const g = groupDataJoin(container, groups);
 
+            const getAxisSize = i => (Array.isArray(tickSizeInner) ? tickSizeInner[i] : tickSizeInner);
+            const getAxisOffset = i => {
+                let sum = 0;
+                for (let n = 0; n < i; n++) {
+                    sum += getAxisSize(n);
+                }
+                return sum;
+            };
+
             // enter
-            g.enter().attr("transform", (d, i) => translate(0, sign * i * multiAxis.tickSizeInner()));
+            g.enter().attr("transform", (d, i) => translate(0, sign * getAxisOffset(i)));
 
             g.each((group, i, nodes) => {
                 const groupElement = select(nodes[i]);
                 const groupScale = scaleFromGroup(scale, group);
                 const useAxis = axisStore(baseAxis(groupScale))
-                    .decorate(decorate)
+                    .decorate((s, data) => decorate(s, data, i))
+                    .tickSizeInner(getAxisSize(i))
                     .tickOffset(d => groupScale.step(d) / 2);
-
                 useAxis(groupElement);
 
                 groupElement.select("path.domain").attr("visibility", "hidden");
             });
 
             // exit
-            g.exit().attr("transform", translate(0, sign * index * multiAxis.tickSizeInner()));
+            g.exit().attr("transform", (d, i) => translate(0, sign * getAxisOffset(i)));
         });
     };
 
@@ -87,6 +97,30 @@ const multiAxis = (orient, baseAxis, scale) => {
 
         rebindAll(customScale, scale, exclude("ticks", "step", "copy"));
         return customScale;
+    };
+
+    multiAxis.tickSize = (...args) => {
+        if (!args.length) {
+            return tickSizeInner;
+        }
+        tickSizeInner = tickSizeOuter = Number(args[0]);
+        return multiAxis;
+    };
+
+    multiAxis.tickSizeInner = (...args) => {
+        if (!args.length) {
+            return tickSizeInner;
+        }
+        tickSizeInner = Array.isArray(args[0]) ? args[0] : Number(args[0]);
+        return multiAxis;
+    };
+
+    multiAxis.tickSizeOuter = (...args) => {
+        if (!args.length) {
+            return tickSizeOuter;
+        }
+        tickSizeOuter = Number(args[0]);
+        return multiAxis;
     };
 
     multiAxis.decorate = (...args) => {
