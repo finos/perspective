c99645194cdefdc22260f75d6d0f01c12054cb8f
Ro4052 <Ro4052@users.noreply.github.com>
2019-03-04 16:06:56 +0000

    Initial implementation of the interpolated legend

--- packages/perspective-viewer-d3fc/src/js/legend/colourRangeLegend.js
+++ packages/perspective-viewer-d3fc/src/js/legend/colourRangeLegend.js
@@ -6,39 +6,80 @@
  * the Apache License 2.0.  The full license can be found in the LICENSE file.
  *
  */
-import {rebindAll} from "d3fc";
-import * as d3Legend from "d3-svg-legend";
+import * as d3 from "d3";
+import * as fc from "d3fc";
 import {getOrCreateElement} from "../utils/utils";
 
 export function colourRangeLegend() {
-    let baseLegend = d3Legend
-        .legendColor()
-        .shapeWidth(30)
-        .cells(10)
-        .orient("vertical")
-        .ascending(true);
+    const width = 100;
+    const height = 150;
+    let scale = null;
+
+    const xScale = d3
+        .scaleBand()
+        .domain([0, 1])
+        .range([0, width]);
 
     function legend(container) {
-        const legendSelection = getOrCreateElement(container, "div.legend-container", () =>
-            container
-                .append("div")
-                .attr("class", "legend-container")
-                .style("z-index", "2")
-                .append("svg")
-                .attr("class", "legend")
-        );
+        const domain = scale.domain();
+        const min = domain[0] - 1;
+        const max = domain[1] + 1;
+        const expandedDomain = d3.range(min, max, (max - min) / height);
 
-        // render the legend
-        legendSelection.call(baseLegend);
+        const yScale = d3
+            .scaleLinear()
+            .domain(fc.extentLinear()(expandedDomain))
+            .range([height, 0]);
 
-        const cellSize = legendSelection
-            .select("g.legendCells")
-            .node()
-            .getBBox();
-        legendSelection.attr("height", cellSize.height + 20);
+        const svgBar = fc
+            .autoBandwidth(fc.seriesSvgBar())
+            .xScale(xScale)
+            .yScale(yScale)
+            .crossValue(0)
+            .baseValue((_, i) => (i > 0 ? expandedDomain[i - 1] : 0))
+            .mainValue(d => d)
+            .decorate(selection => {
+                selection.selectAll("path").style("fill", d => scale(d));
+            });
+
+        const axisLabel = fc
+            .axisRight(yScale)
+            .tickValues([...domain, (domain[1] + domain[0]) / 2])
+            .tickSizeOuter(0)
+            .tickFormat(d => {
+                const formatFunc = d === 1 || d % 2 === 0 ? d3.format(",.0f") : d3.format(",.2f");
+                return formatFunc(d);
+            });
+
+        const legendSelection = getOrCreateElement(container, "div.legend-container", () => container.append("div"));
+
+        const legendSvg = legendSelection
+            .attr("class", "legend-container")
+            .style("z-index", "2")
+            .append("svg")
+            .style("width", width)
+            .style("height", height);
+        const legendBar = legendSvg
+            .append("g")
+            .datum(expandedDomain)
+            .call(svgBar);
+
+        const barWidth = Math.abs(legendBar.node().getBBox().x);
+        legendSvg
+            .append("svg")
+            .append("g")
+            .attr("transform", `translate(${barWidth})`)
+            .datum(expandedDomain)
+            .call(axisLabel);
     }
 
-    rebindAll(legend, baseLegend);
+    legend.scale = (...args) => {
+        if (!args.length) {
+            return scale;
+        }
+        scale = args[0];
+        return legend;
+    };
 
     return legend;
 }
