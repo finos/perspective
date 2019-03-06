73db153bbc9177f66d2f3285bc4b870cbcf97295
Sam Sutherland Grant <redbear.sam@hotmail.com>
2019-03-06 10:51:16 +0000

    y_scatter makes use of d3 shapes for points

---
+++ packages/perspective-viewer-d3fc/src/js/series/categoryPointSeries.js
@@ -0,0 +1,46 @@
+/******************************************************************************
+ *
+ * Copyright (c) 2017, the Perspective Authors.
+ *
+ * This file is part of the Perspective library, distributed under the terms of
+ * the Apache License 2.0.  The full license can be found in the LICENSE file.
+ *
+ */
+import * as d3 from "d3";
+import * as fc from "d3fc";
+import {tooltip} from "../tooltip/tooltip";
+import {withoutOpacity} from "./seriesColours";
+
+const symbols = [d3.symbolCircle, d3.symbolCross, d3.symbolDiamond, d3.symbolSquare, d3.symbolStar, d3.symbolTriangle, d3.symbolWye];
+
+export function categoryPointSeries(settings, seriesKey, colour, symbols) {
+    let series = fc.seriesSvgPoint().size(100);
+
+    if (symbols) {
+        series.type(symbols(seriesKey));
+    }
+
+    series.decorate(selection => {
+        tooltip()(selection, settings);
+        if (colour) {
+            selection.style("stroke", d => withoutOpacity(colour(d.colorValue || seriesKey))).style("fill", d => colour(d.colorValue || seriesKey));
+        }
+    });
+
+    return series.crossValue(d => d.crossValue).mainValue(d => d.mainValue);
+}
+
+export function symbolType(settings) {
+    const col = settings.data && settings.data.length > 0 ? settings.data[0] : {};
+    const domain = Object.keys(col).filter(k => k !== "__ROW_PATH__");
+    return fromDomain(domain);
+}
+
+function fromDomain(domain) {
+    return domain.length > 1
+        ? d3
+              .scaleOrdinal()
+              .domain(domain)
+              .range(symbols)
+        : null;
+}
