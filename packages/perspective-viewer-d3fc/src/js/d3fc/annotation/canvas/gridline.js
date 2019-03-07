6565f169102f4b54caeb09c094c787e65bb6410a
Ro4052 <Ro4052@users.noreply.github.com>
2019-03-07 16:15:42 +0000

    Added basic canvas gridlines using potential d3fc component

---
+++ packages/perspective-viewer-d3fc/src/js/d3fc/annotation/canvas/gridline.js
@@ -0,0 +1,71 @@
+import {line as lineShape} from "d3-shape";
+import ticks from "../ticks";
+import {prefix, rebindAll, rebind} from "@d3fc/d3fc-rebind";
+
+export default () => {
+    let xDecorate = () => {};
+    let yDecorate = () => {};
+
+    const xTicks = ticks();
+    const yTicks = ticks();
+
+    const lineData = lineShape();
+
+    const instance = () => {
+        const context = lineData.context();
+        const xScale = xTicks.scale();
+        const yScale = yTicks.scale();
+
+        xScale.ticks().forEach(xTick => {
+            context.save();
+            context.beginPath();
+            context.strokeStyle = "#bbb";
+            context.fillStyle = "transparent";
+
+            yDecorate(context);
+            lineData.context(context)(yScale.domain().map(d => [xScale(xTick), yScale(d)]));
+
+            context.fill();
+            context.stroke();
+            context.closePath();
+            context.restore();
+        });
+
+        yScale.ticks().forEach(yTick => {
+            context.save();
+            context.beginPath();
+            context.strokeStyle = "#bbb";
+            context.fillStyle = "transparent";
+
+            yDecorate(context);
+            lineData.context(context)(yScale.domain().map(d => [xScale(d), yScale(yTick)]));
+
+            context.fill();
+            context.stroke();
+            context.closePath();
+            context.restore();
+        });
+    };
+
+    instance.yDecorate = (...args) => {
+        if (!args.length) {
+            return yDecorate;
+        }
+        yDecorate = args[0];
+        return instance;
+    };
+
+    instance.xDecorate = (...args) => {
+        if (!args.length) {
+            return xDecorate;
+        }
+        xDecorate = args[0];
+        return instance;
+    };
+
+    rebindAll(instance, xTicks, prefix("x"));
+    rebindAll(instance, yTicks, prefix("y"));
+    rebind(instance, lineData, "context");
+
+    return instance;
+};
