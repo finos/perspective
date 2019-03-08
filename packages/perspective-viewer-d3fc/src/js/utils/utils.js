f75ea2726e8fcffba234b7d3be0966cc9c4bfc46
Matthew Hooper <mhooper@scottlogic.co.uk>
2019-03-08 16:59:22 +0000

    Tooltips for line charts

--- packages/perspective-viewer-d3fc/src/js/utils/utils.js
+++ packages/perspective-viewer-d3fc/src/js/utils/utils.js
@@ -1,3 +1,12 @@
+/******************************************************************************
+ *
+ * Copyright (c) 2017, the Perspective Authors.
+ *
+ * This file is part of the Perspective library, distributed under the terms of
+ * the Apache License 2.0.  The full license can be found in the LICENSE file.
+ *
+ */
+
 export function areArraysEqualSimple(arr1, arr2) {
     return JSON.stringify(arr1) === JSON.stringify(arr2);
 }
@@ -5,4 +14,14 @@ export function areArraysEqualSimple(arr1, arr2) {
 export function getOrCreateElement(container, selector, createCallback) {
     let element = container.select(selector);
     return element.size() > 0 ? element : createCallback();
+}
+
+export function getSVGPlotAreaValues(container) {
+    const plotArea = container.select(".plot-area");
+    const yAxis = container.select("d3fc-svg.y-axis");
+    const xOffset = yAxis._groups[0][0].offsetWidth + yAxis._groups[0][0].offsetLeft;
+    const {offsetTop, clientHeight} = plotArea._groups[0][0];
+    const yOffset = offsetTop;
+
+    return {xOffset, yOffset, clientHeight};
 }
