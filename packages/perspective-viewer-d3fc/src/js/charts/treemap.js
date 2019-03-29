9f40d56283282631895897d9344c71865b243042
matt-hooper <mhooper@scottlogic.com>
2019-03-29 17:28:17 +0000

    add drill-down

--- packages/perspective-viewer-d3fc/src/js/charts/treemap.js
+++ packages/perspective-viewer-d3fc/src/js/charts/treemap.js
@@ -9,6 +9,9 @@
 
 import * as d3 from "d3";
 import {treeData} from "../data/treeData";
+import {getOrCreateElement} from "../utils/utils";
+import treemapLayout from "../layout/treemapLayout";
+import template from "../../html/parent-controls.html";
 
 function getColors(nodes, colors = []) {
     if (nodes.children && nodes.children.length > 0) {
@@ -21,8 +24,8 @@ function getColors(nodes, colors = []) {
     return colors;
 }
 
-function treeColor(settings, data) {
-    if (settings.mainValues.length > 1) {
+function treeColor(data, maxDepth) {
+    if (maxDepth > 0) {
         const colors = getColors(data);
         const min = Math.min(...colors);
         const max = Math.max(...colors);
@@ -33,10 +36,13 @@ function treeColor(settings, data) {
 }
 
 const addTreemap = (selection, data, settings) => {
-    console.log("data", data);
-    const color = treeColor(settings, data);
+    const maxDepth = data.height;
+    const color = treeColor(data, maxDepth);
+    let parent = null;
 
-    const isDeepest = d => d.depth === settings.crossValues.length;
+    const isDeepest = d => d.depth === maxDepth;
+    const isTop = d => d.depth === 1;
+    const showName = d => maxDepth === 0 || (0 < d.depth && d.depth <= 2);
 
     const nodes = selection
         .select("svg g")
@@ -46,39 +52,62 @@ const addTreemap = (selection, data, settings) => {
         .append("g")
         .attr("transform", d => `translate(${[d.x0, d.y0]})`);
 
+    // draw child nodes first
+    nodes.sort((a, b) => (b.depth === 0 ? 0 : b.depth - a.depth));
+
     nodes
         .append("rect")
         .attr("class", "treerect")
         .attr("width", d => d.x1 - d.x0)
         .attr("height", d => d.y1 - d.y0)
         .attr("fill", d => color(d.data.color))
-        .attr("fill-opacity", d => (isDeepest(d) ? 0.8 : 0));
+        .attr("fill-opacity", d => (isDeepest(d) ? 0.8 : 0))
+        .on("click", d => {
+            transition(d);
+        });
 
     nodes
         .append("text")
-        .attr("class", d => (d.depth === 1 ? "top" : "lower"))
+        .attr("class", d => (isTop(d) ? "top" : "lower"))
         .attr("dx", d => (d.x1 - d.x0) / 2)
         .attr("dy", d => (d.y1 - d.y0) / 2)
-        .text(d => (0 < d.depth && d.depth <= 2 ? d.data.name : ""));
+        .text(d => (showName(d) ? d.data.name : ""));
+
+    const transition = d => {
+        if (!d || d.height === 0) return;
+
+        selection
+            .select("svg g")
+            .selectAll("g")
+            .remove();
+
+        let newData = d3.hierarchy(d.data).sum(d => d.size);
+
+        parent = d.parent;
+
+        const {width: containerWidth, height: containerHeight} = selection.node().getBoundingClientRect();
+
+        treemapLayout(containerWidth, containerHeight)(newData);
+        newData.parent = d.parent;
+
+        addTreemap(selection, newData, settings);
+
+        getGoToParentControls(selection)
+            .style("display", parent ? "" : "none")
+            .select("#goto-parent")
+            .html(newData ? newData.data.name : "")
+            .on("click", () => {
+                transition(parent);
+            });
+    };
 };
 
 function treemap(container, settings) {
-    const padder = d => {
-        return settings.crossValues.length - d.depth;
-    };
-
-    const padding = 30;
     const {width: containerWidth, height: containerHeight} = container.node().getBoundingClientRect();
 
     const {data} = treeData(settings)[0];
 
-    const treemapLayout = d3
-        .treemap()
-        .size([containerWidth - padding, containerHeight - padding])
-        .paddingInner(d => padder(d));
-
-    treemapLayout.tile(d3.treemapBinary);
-    treemapLayout(data);
+    treemapLayout(containerWidth, containerHeight)(data);
 
     container
         .append("svg")
@@ -88,6 +117,16 @@ function treemap(container, settings) {
 
     addTreemap(container, data, settings);
 }
+
+const getGoToParentControls = container =>
+    getOrCreateElement(container, ".parent-controls", () =>
+        container
+            .append("div")
+            .attr("class", "parent-controls")
+            .style("display", "none")
+            .html(template)
+    );
+
 treemap.plugin = {
     type: "d3_treemap",
     name: "[D3] Treemap",
