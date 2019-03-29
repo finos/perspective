26a1826987ce22763846b60acb447c49aa1239bc
matt-hooper <mhooper@scottlogic.com>
2019-03-29 11:20:14 +0000

    Add rect styles to css

--- packages/perspective-viewer-d3fc/src/js/charts/treemap.js
+++ packages/perspective-viewer-d3fc/src/js/charts/treemap.js
@@ -10,28 +10,20 @@
 import * as d3 from "d3";
 import {treeData} from "../data/treeData";
 
-const base_font_size = 12;
-
-function getColors(nodes, colors) {
+function getColors(nodes, colors = []) {
     if (nodes.children && nodes.children.length > 0) {
         nodes.children.forEach(child => {
-            const childColor = getColors(child, colors);
-            if (Array.isArray(childColor)) {
-                colors.concat(childColor);
-            } else {
-                colors.push(childColor);
-            }
+            colors.concat(getColors(child, colors));
         });
-
-        return colors;
     } else {
-        return nodes.data.color;
+        colors.push(nodes.data.color);
     }
+    return colors;
 }
 
 function treeColor(settings, data) {
     if (settings.mainValues.length > 1) {
-        const colors = getColors(data, []);
+        const colors = getColors(data);
         const min = Math.min(...colors);
         const max = Math.max(...colors);
         return d3.scaleSequential(d3.interpolateViridis).domain([min, max]);
@@ -40,19 +32,11 @@ function treeColor(settings, data) {
     return () => "rgb(31, 119, 180)";
 }
 
-const addTreemap = (selection, data, color, settings) => {
+const addTreemap = (selection, data, settings) => {
     console.log("data", data);
-    //console.log("color domain", color.domain());
+    const color = treeColor(settings, data);
 
-    const treeColorScale = treeColor(settings, data);
-
-    //console.log("treeColorScale domain", treeColorScale.domain());
-
-    const adjustFontSize = d => {
-        return `${base_font_size + 2 * settings.crossValues.length - 2 * d.depth}px`;
-    };
-
-    const isTop = d => d.depth === settings.crossValues.length;
+    const isDeepest = d => d.depth === settings.crossValues.length;
 
     const nodes = selection
         .select("svg g")
@@ -60,36 +44,22 @@ const addTreemap = (selection, data, color, settings) => {
         .data(data.descendants())
         .enter()
         .append("g")
-        .attr("transform", function(d) {
-            return `translate(${[d.x0, d.y0]})`;
-        });
+        .attr("transform", d => `translate(${[d.x0, d.y0]})`);
 
     nodes
         .append("rect")
         .attr("class", "treerect")
-        .attr("width", function(d) {
-            return d.x1 - d.x0;
-        })
-        .attr("height", function(d) {
-            return d.y1 - d.y0;
-        })
-        .attr("fill", d => (isTop(d) ? treeColorScale(d.data.color) : "none"))
-        .attr("fill-opacity", d => (isTop(d) ? 0.8 : 0));
+        .attr("width", d => d.x1 - d.x0)
+        .attr("height", d => d.y1 - d.y0)
+        .attr("fill", d => color(d.data.color))
+        .attr("fill-opacity", d => (isDeepest(d) ? 0.8 : 0));
 
     nodes
         .append("text")
-        .attr("dx", function(d) {
-            return (d.x1 - d.x0) / 2;
-        })
-        .attr("dy", function(d) {
-            return (d.y1 - d.y0) / 2;
-        })
-        .attr("font-size", function(d) {
-            return adjustFontSize(d);
-        })
-        .text(function(d) {
-            return 0 < d.depth && d.depth <= 2 ? d.data.name + ": " + d.data.color : "";
-        });
+        .attr("class", d => (d.depth === 1 ? "top" : "lower"))
+        .attr("dx", d => (d.x1 - d.x0) / 2)
+        .attr("dy", d => (d.y1 - d.y0) / 2)
+        .text(d => (0 < d.depth && d.depth <= 2 ? d.data.name : ""));
 };
 
 function treemap(container, settings) {
@@ -100,7 +70,7 @@ function treemap(container, settings) {
     const padding = 30;
     const {width: containerWidth, height: containerHeight} = container.node().getBoundingClientRect();
 
-    const {data, color} = treeData(settings)[0];
+    const {data} = treeData(settings)[0];
 
     const treemapLayout = d3
         .treemap()
@@ -116,7 +86,7 @@ function treemap(container, settings) {
         .attr("height", "100%")
         .append("g");
 
-    addTreemap(container, data, color, settings);
+    addTreemap(container, data, settings);
 }
 treemap.plugin = {
     type: "d3_treemap",
