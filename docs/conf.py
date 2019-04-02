e26e44a6d30cec6c34430c09be86974db9c13249
Tim Paine <timothy.k.paine@jpmorgan.com>
2019-04-02 13:04:37 -0400

    give up on yarn

--- docs/conf.py
+++ docs/conf.py
@@ -35,11 +35,9 @@ release = '0.2.14'  # FIXME
 
 if os.environ.get('READTHEDOCS', None):
     print('****\nDetected READTHEDOCS Environment\n****\n')
-    subprocess.call('npm install yarn --save-dev', shell=True)
     subprocess.call('npm install fast-async @babel/cli @babel/core @babel/plugin-proposal-decorators @babel/plugin-transform-for-of  @babel/preset-env babel-plugin-transform-custom-element-classes jsdoc jsdoc-babel --save-dev', shell=True)
-    print(os.listdir('.'))
-    subprocess.call('../node_modules/.bin/yarn')
-    subprocess.call('../node_modules/.bin/yarn autodocs')
+    subprocess.call('npm install')
+    subprocess.call('npm run autodocs')
 
 # -- General configuration ---------------------------------------------------
 
