b5ce38325865cf6601d16955279963d895b81472
Sam Sutherland Grant <redbear.sam@hotmail.com>
2019-04-03 15:56:11 +0100

    handle the case where a max dimension is smaller than a min dimension and vice versa

--- packages/perspective-viewer-d3fc/src/js/legend/styling/resizableComponent.js
+++ packages/perspective-viewer-d3fc/src/js/legend/styling/resizableComponent.js
@@ -157,11 +157,13 @@ export function resizableComponent() {
 
     resizable.minWidth = input => {
         minDimensionsPx.width = input;
+        if (!!maxDimensionsPx.width && maxDimensionsPx.width < minDimensionsPx.width) maxDimensionsPx.width = minDimensionsPx.width;
         return resizable;
     };
 
     resizable.minHeight = input => {
         minDimensionsPx.height = input;
+        if (!!maxDimensionsPx.height && maxDimensionsPx.height < minDimensionsPx.height) maxDimensionsPx.height = minDimensionsPx.height;
         return resizable;
     };
 
@@ -172,11 +174,13 @@ export function resizableComponent() {
 
     resizable.maxWidth = input => {
         maxDimensionsPx.width = input;
+        if (minDimensionsPx.width > maxDimensionsPx.width) minDimensionsPx.width = maxDimensionsPx.width;
         return resizable;
     };
 
     resizable.maxHeight = input => {
         maxDimensionsPx.height = input;
+        if (minDimensionsPx.height > maxDimensionsPx.height) minDimensionsPx.height = maxDimensionsPx.height;
         return resizable;
     };
 
