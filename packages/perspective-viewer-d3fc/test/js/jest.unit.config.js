8e125b331b306f7ad92414a38a69e83bff3ef6d7
Andy <alee@scottlogic.com>
2019-04-03 16:28:15 +0100

    Implemented an example UI/D3 based unit test
    The test is able to create UI elements and then test that what was created is correct.
    Also added a new `yarn test:unit` command to allow you to easily run just the unit tests in watch mode.

---
+++ packages/perspective-viewer-d3fc/test/js/jest.unit.config.js
@@ -0,0 +1,19 @@
+/******************************************************************************
+ *
+ * Copyright (c) 2017, the Perspective Authors.
+ *
+ * This file is part of the Perspective library, distributed under the terms of
+ * the Apache License 2.0.  The full license can be found in the LICENSE file.
+ *
+ */
+
+module.exports = {
+    roots: ["unit"],
+    verbose: true,
+    testURL: "http://localhost/",
+    automock: false,
+    transform: {
+        ".js$": "./transform.js"
+    },
+    setupFiles: ["./beforeEachSpec.js"]
+};
