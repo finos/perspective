47c960cb4f44fc3c858d09b6c05026386b188d72
matt-hooper <mhooper@scottlogic.com>
2019-05-03 09:04:51 +0100

    Add save and restore back in

--- packages/perspective-viewer-d3fc/src/js/plugin/plugin.js
+++ packages/perspective-viewer-d3fc/src/js/plugin/plugin.js
@@ -30,7 +30,9 @@ export function register(...plugins) {
                 ...chart.plugin,
                 create: drawChart(chart),
                 resize: resizeChart,
-                delete: deleteChart
+                delete: deleteChart,
+                save,
+                restore
             });
         }
     });
