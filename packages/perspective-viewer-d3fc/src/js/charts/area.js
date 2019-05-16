d486a1aefaf0454f1c32d2917e450b966d5cb1fc
Andy <alee@scottlogic.com>
2019-05-16 12:18:10 +0100

    Added y-axis split to area, y-scatter and column charts
    Column chart needs to blank-out instead of filter the data for each y-axis, and also needs to harmonise the origin of the domain on each side (to make the bars line up).

--- packages/perspective-viewer-d3fc/src/js/charts/area.js
+++ packages/perspective-viewer-d3fc/src/js/charts/area.js
@@ -9,6 +9,7 @@
 import * as fc from "d3fc";
 import {axisFactory} from "../axis/axisFactory";
 import {chartSvgFactory} from "../axis/chartFactory";
+import {axisSplitter} from "../axis/axisSplitter";
 import {AXIS_TYPES} from "../axis/axisType";
 import {areaSeries} from "../series/areaSeries";
 import {seriesColors} from "../series/seriesColors";
@@ -35,15 +36,25 @@ function areaChart(container, settings) {
         .excludeType(AXIS_TYPES.linear)
         .settingName("crossValues")
         .valueName("crossValue")(data);
-    const yAxis = axisFactory(settings)
+    const yAxisFactory = axisFactory(settings)
         .settingName("mainValues")
         .valueName("mainValue")
         .excludeType(AXIS_TYPES.ordinal)
         .orient("vertical")
         .include([0])
-        .paddingStrategy(hardLimitZeroPadding())(data);
+        .paddingStrategy(hardLimitZeroPadding());
 
-    const chart = chartSvgFactory(xAxis, yAxis).plotArea(withGridLines(series).orient("vertical"));
+    // Check whether we've split some values into a second y-axis
+    const splitter = axisSplitter(settings, data).color(color);
+
+    const yAxis1 = yAxisFactory(splitter.data());
+
+    // No grid lines if splitting y-axis
+    const plotSeries = splitter.haveSplit() ? series : withGridLines(series).orient("vertical");
+
+    const chart = chartSvgFactory(xAxis, yAxis1)
+        .axisSplitter(splitter)
+        .plotArea(plotSeries);
 
     chart.yNice && chart.yNice();
 
@@ -55,12 +66,20 @@ function areaChart(container, settings) {
     const toolTip = nearbyTip()
         .settings(settings)
         .xScale(xAxis.scale)
-        .yScale(yAxis.scale)
+        .yScale(yAxis1.scale)
         .color(color)
         .data(data);
 
+    if (splitter.haveSplit()) {
+        // Create the y-axis data for the alt-axis
+        const yAxis2 = yAxisFactory(splitter.altData());
+        chart.altAxis(yAxis2);
+        // Give the tooltip the information (i.e. 2 datasets with different scales)
+        toolTip.data(splitter.data()).altDataWithScale({yScale: yAxis2.scale, data: splitter.altData()});
+    }
+
     // render
-    container.datum(data).call(zoomChart);
+    container.datum(splitter.data()).call(zoomChart);
     container.call(toolTip);
     container.call(legend);
 }
