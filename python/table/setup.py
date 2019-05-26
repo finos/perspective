c1292d603167f065cd4c11ad22f5e2fdc286ddac
Tim Paine <t.paine154@gmail.com>
2019-05-26 13:35:57 -0400

    simplify python setup.py, add psp_test to python tests

--- python/table/setup.py
+++ python/table/setup.py
@@ -5,86 +5,94 @@
 # This file is part of the Perspective library, distributed under the terms of
 # the Apache License 2.0.  The full license can be found in the LICENSE file.
 #
+from setuptools import setup, Extension
+from distutils.version import LooseVersion
+from codecs import open
+from setuptools.command.build_ext import build_ext
 import os
 import os.path
-import fnmatch
-import pathlib
-from shutil import copy
-from setuptools import setup, Extension
-from setuptools.command.build_ext import build_ext as build_ext_orig
-from codecs import open
+import os
+import re
+import sys
+import platform
+import subprocess
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+with open(os.path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    requires = f.read().split()
+
+if sys.version_info.major < 3 or sys.version_info.minor < 7:
+    raise Exception('Must be python3.7 or above')
+
 
 class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
 
-    def __init__(self, name):
-        # don't invoke the original build_ext for this special extension
-        super().__init__(name, sources=[])
 
-
-class build_ext(build_ext_orig):
-
+class CMakeBuild(build_ext):
     def run(self):
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError(
+                "CMake must be installed to build the following extensions: " +
+                ", ".join(e.name for e in self.extensions))
+
+        if platform.system() == "Windows":
+            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)',
+                                         out.decode()).group(1))
+            if cmake_version < '3.1.0':
+                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
+
         for ext in self.extensions:
-            self.build_cmake(ext)
-        super().run()
+            self.build_extension(ext)
 
-    def build_cmake(self, ext):
-        cwd = pathlib.Path().absolute()
-        directory = os.path.abspath(os.path.join(pathlib.Path().absolute(), "..", "..", "cpp", "perspective"))
+    def build_extension(self, ext):
+        extdir = os.path.abspath(
+            os.path.dirname(self.get_ext_fullpath(ext.name)))
+        cfg = 'Debug' if self.debug else 'Release'
 
-        # these dirs will be created in build_py, so if you don't have
-        # any python sources to bundle, the dirs will be missing
-        build_temp = pathlib.Path(self.build_temp)
-        build_temp.mkdir(parents=True, exist_ok=True)
-        extdir = pathlib.Path(self.get_ext_fullpath(ext.name))
-        extdir.mkdir(parents=True, exist_ok=True)
-
-        # example of cmake args
-        config = 'Debug' if self.debug else 'Release'
         cmake_args = [
-            '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + str(os.path.join(extdir.parent.absolute(), 'perspective', 'table')),
-            '-DCMAKE_BUILD_TYPE=' + config,
+            '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + str(os.path.join(extdir, 'perspective', 'table')),
+            '-DCMAKE_BUILD_TYPE=' + cfg,
             '-DPSP_CPP_BUILD=1',
             '-DPSP_WASM_BUILD=0',
             '-DPSP_PYTHON_BUILD=1',
+            '-DPSP_CPP_BUILD_TESTS=1'
         ]
 
-        # example of build args
-        build_args = [
-            '--config', config,
-            '--', '-j2' if os.environ.get('DOCKER', '') else '-j4',
-        ]
-        os.environ['PSP_ENABLE_PYTHON'] = '1'
+        build_args = ['--config', cfg]
 
-        print(str(build_temp))
-        print(str(directory))
-        os.chdir(str(build_temp))
-        self.spawn(['cmake', str(directory)] + cmake_args)
-        if not self.dry_run:
-            self.spawn(['cmake', '--build', '.', ] + build_args)
-        os.chdir(str(cwd))
+        if platform.system() == "Windows":
+            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(
+                cfg.upper(),
+                extdir)]
+            if sys.maxsize > 2**32:
+                cmake_args += ['-A', 'x64']
+            build_args += ['--', '/m']
+        else:
+            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+            build_args += ['--', '-j2' if os.environ.get('DOCKER', '') else '-j4']
 
-        def find(pattern, path):
-            for root, dirs, files in os.walk(path):
-                for name in files:
-                    if fnmatch.fnmatch(name, pattern):
-                        return os.path.join(root, name)
+        env = os.environ.copy()
+        env['PSP_ENABLE_PYTHON'] = '1'
 
-        binding = find('libbinding.*', 'build')
-        library = find('libpsp.*', 'build')
-
-        print("copying libbinding.so")
-        copy(binding, 'perspective/table')
-
-        print("copying libpsp.so")
-        copy(library, 'perspective/table')
-
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(
+            env.get('CXXFLAGS', ''),
+            self.distribution.get_version())
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        subprocess.check_call(['cmake', os.path.abspath(os.path.join(ext.sourcedir, '..', '..', 'cpp', 'perspective'))] + cmake_args,
+                              cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args,
+                              cwd=self.build_temp)
+        print()  # Add an empty line for cleaner output
 
 setup(
     name='perspective-python.table',
@@ -92,11 +100,16 @@ setup(
     description='Analytics library',
     long_description=long_description,
     url='https://github.com/finos/perspective',
+    download_url='https://github.com/timkpaine/aat/archive/v0.0.2.tar.gz',
     author='Tim Paine',
     author_email='timothy.k.paine@gmail.com',
     license='Apache 2.0',
+    install_requires=requires,
+    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
@@ -107,11 +120,11 @@ setup(
 
     keywords='analytics tools plotting',
     packages=['perspective.table'],
+    include_package_data=True,
     zip_safe=False,
+    extras_require={'dev': requires + ['pytest', 'pytest-cov', 'pylint', 'flake8', 'mock']},
     ext_modules=[
-        CMakeExtension('perspective')
+        CMakeExtension('perspective'),
     ],
-    cmdclass={
-        'build_ext': build_ext,
-    }
+    cmdclass=dict(build_ext=CMakeBuild),
 )
