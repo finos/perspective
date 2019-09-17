a06f3d46d8850d6f2e09fa80b5a5aff17e8ff604
Jun Tan <tanjunyuan@hotmail.co.uk>
2019-09-17 11:25:49 -0400

    fix python signatures for set_column_nth

--- python/table/perspective/include/perspective/python/fill.h
+++ python/table/perspective/include/perspective/python/fill.h
@@ -22,36 +22,36 @@ namespace binding {
  * Fill columns with data
  */
 
-// void
-// _fill_col_time(t_data_accessor accessor, std::shared_ptr<t_column> col, std::string name,
-//     std::int32_t cidx, t_dtype type, bool is_arrow, bool is_update) {
-
-// void
-// _fill_col_date(t_data_accessor accessor, std::shared_ptr<t_column> col, std::string name,
-//     std::int32_t cidx, t_dtype type, bool is_arrow, bool is_update) {
-
+void _fill_col_time(t_data_accessor accessor, std::shared_ptr<t_column> col, std::string name, std::int32_t cidx, t_dtype type, bool is_arrow, bool is_update);
+void _fill_col_date(t_data_accessor accessor, std::shared_ptr<t_column> col, std::string name, std::int32_t cidx, t_dtype type, bool is_arrow, bool is_update);
 void _fill_col_bool(t_data_accessor accessor, std::shared_ptr<t_column> col, std::string name, std::int32_t cidx, t_dtype type, bool is_arrow, bool is_update);
 void _fill_col_string(t_data_accessor accessor, std::shared_ptr<t_column> col, std::string name, std::int32_t cidx, t_dtype type, bool is_arrow, bool is_update);
 void _fill_col_int64(t_data_accessor accessor, t_data_table& tbl, std::shared_ptr<t_column> col, std::string name, std::int32_t cidx, t_dtype type, bool is_arrow, bool is_update);
-
-
-template <>
-void set_column_nth(t_column* col, t_uindex idx, t_val value);
-
-// TODO
-// template <>
-// void
-// table_add_computed_column(t_data_table& table, t_val computed_defs) {
-
 void _fill_col_numeric(t_data_accessor accessor, t_data_table& tbl, std::shared_ptr<t_column> col, std::string name, std::int32_t cidx, t_dtype type, bool is_arrow, bool is_update);
 void _fill_data_helper(t_data_accessor accessor, t_data_table& tbl, std::shared_ptr<t_column> col, std::string name, std::int32_t cidx, t_dtype type, bool is_arrow, bool is_update);
 
+
+template <>
+void set_column_nth(std::shared_ptr<t_column> col, t_uindex idx, t_val value);
 /******************************************************************************
  *
  * Fill tables with data
  */
 
 void _fill_data(t_data_table& tbl, t_data_accessor accessor, const t_schema& input_schema, const std::string& index, std::uint32_t offset, std::uint32_t limit, bool is_arrow, bool is_update);
+
+/******************************************************************************
+ *
+ * Add computed columns
+ */
+
+/*
+template <>
+void add_computed_column(std::shared_ptr<t_data_table> table, const std::vector<t_uindex>& row_indices, t_val computed_def);
+
+template <>
+void make_computed_lambdas(std::vector<t_val> computed)
+*/
 
 } //namespace binding
 } //namespace perspective
