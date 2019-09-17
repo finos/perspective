05e16fbc8cfc09869ed80c30a3d83d03747dddb8
Tim Paine <timothy.k.paine@jpmorgan.com>
2019-09-17 00:42:30 +0000

    remove lantern/cufflinks dep

--- python/perspective/perspective/tests/core/data/test_pd.py
+++ python/perspective/perspective/tests/core/data/test_pd.py
@@ -5,11 +5,77 @@
 # This file is part of the Perspective library, distributed under the terms of
 # the Apache License 2.0.  The full license can be found in the LICENSE file.
 #
+import string
 from datetime import date, datetime
 import pandas as pd
 import numpy as np
-import lantern as l
 from perspective import PerspectiveWidget
+from random import random, randint, choice
+from faker import Faker
+fake = Faker()
+
+
+def superstore(count=10):
+    data = []
+    for id in range(count):
+        dat = {}
+        dat['Row ID'] = id
+        dat['Order ID'] = fake.ein()
+        dat['Order Date'] = fake.date_this_year()
+        dat['Ship Date'] = fake.date_between_dates(dat['Order Date']).strftime('%Y-%m-%d')
+        dat['Order Date'] = dat['Order Date'].strftime('%Y-%m-%d')
+        dat['Ship Mode'] = choice(['First Class', 'Standard Class', 'Second Class'])
+        dat['Ship Mode'] = choice(['First Class', 'Standard Class', 'Second Class'])
+        dat['Customer ID'] = fake.license_plate()
+        dat['Segment'] = choice(['A', 'B', 'C', 'D'])
+        dat['Country'] = 'US'
+        dat['City'] = fake.city()
+        dat['State'] = fake.state()
+        dat['Postal Code'] = fake.zipcode()
+        dat['Region'] = choice(['Region %d' % i for i in range(5)])
+        dat['Product ID'] = fake.bban()
+        sector = choice(['Industrials', 'Technology', 'Financials'])
+        industry = choice(['A', 'B', 'C'])
+        dat['Category'] = sector
+        dat['Sub-Category'] = industry
+        dat['Sales'] = randint(1, 100) * 100
+        dat['Quantity'] = randint(1, 100) * 10
+        dat['Discount'] = round(random() * 100, 2)
+        dat['Profit'] = round(random() * 1000, 2)
+        data.append(dat)
+    return pd.DataFrame(data)
+
+
+# remove cufflinks dep
+def getName(n=1, name=3, exchange=2, columns=None, mode='abc'):
+    def get_abc(n):
+        def _w(n, base=2):
+            _n = 1
+            st = []
+            while base ** _n <= n:
+                _n += 1
+            for _ in range(_n-1, 0, -1):
+                n_st = n // (base ** _)
+                st.append(n_st)
+                n = n - n_st * (base ** _)
+            st.append(n+1)
+            return st
+        st = _w(n, len(string.ascii_lowercase))
+        _st = ''
+        for _ in st:
+            _st += string.ascii_lowercase[_-1]
+        return _st
+    columns = [get_abc(_) for _ in range(n)]
+    return columns
+
+
+# remove cufflinks dep
+def lines(n_traces=5, n=100, columns=None, dateIndex=True, mode=None):
+    index = pd.date_range('1/1/15', periods=n) if dateIndex else list(range(n))
+    df = pd.DataFrame(np.random.randn(n, n_traces), index=index,
+                      columns=getName(n_traces, columns=columns, mode=mode))
+    return df.cumsum()
+
 
 DF2 = df = pd.DataFrame([
     {
@@ -30,8 +96,8 @@ DF2 = df = pd.DataFrame([
     },
 ])
 
-DF = l.superstore()
-LINE = l.line()
+DF = superstore()
+LINE = lines()
 CUSTOM_SCHEMA = {'int': 'int', 'float': 'float', 'string': 'date', 'date': 'date', 'datetime': 'date', 'object': 'string'}
 CUSTOM_SCHEMA_CONVERTED = {'int': 'integer', 'float': 'float', 'string': 'date', 'date': 'date', 'datetime': 'date', 'object': 'string'}
 
@@ -58,11 +124,9 @@ class TestPandas:
         psp = PerspectiveWidget(pt)
         assert psp.rowpivots == ['Country', 'Region']
         assert psp.columnpivots == []
-        assert psp.columns == ['Consumer Discretionary', 'Consumer Staples', 'Energy', 'Financials',
-                               'Health Care', 'Industrials', 'Information Technology', 'Materials', 'Real Estate', 'Telecommunication Services', 'Utilities']
-        assert psp.schema == {'Country': 'string', 'Region': 'string', 'Consumer Discretionary': 'float', 'Consumer Staples': 'float', 'Energy': 'float', 'Financials': 'float',
-                              'Health Care': 'float', 'Industrials': 'float', 'Information Technology': 'float', 'Materials': 'float', 'Real Estate': 'float',
-                              'Telecommunication Services': 'float', 'Utilities': 'float'}
+        assert psp.columns == ['Financials', 'Industrials', 'Technology']
+        assert psp.schema == {'Country': 'string', 'Region': 'string', 'Financials': 'float',
+                              'Industrials': 'float', 'Technology': 'float'}
 
     def test_colpivots(self):
         arrays = [np.array(['bar', 'bar', 'bar', 'bar', 'baz', 'baz', 'baz', 'baz', 'foo', 'foo', 'foo', 'foo', 'qux', 'qux', 'qux', 'qux']),
--- python/perspective/perspective/tests/table/test_table_pandas.py
+++ python/perspective/perspective/tests/table/test_table_pandas.py
@@ -10,78 +10,75 @@ import numpy as np
 from perspective.table import Table
 from random import random, randint, choice
 from faker import Faker
+import pandas as pd
 fake = Faker()
 
-try:
-    import pandas as pd
 
-    def superstore(count=10):
-        data = []
-        for id in range(count):
-            dat = {}
-            dat['Row ID'] = id
-            dat['Order ID'] = fake.ein()
-            dat['Order Date'] = fake.date_this_year()
-            dat['Ship Date'] = fake.date_between_dates(dat['Order Date']).strftime('%Y-%m-%d')
-            dat['Order Date'] = dat['Order Date'].strftime('%Y-%m-%d')
-            dat['Ship Mode'] = choice(['First Class', 'Standard Class', 'Second Class'])
-            dat['Ship Mode'] = choice(['First Class', 'Standard Class', 'Second Class'])
-            dat['Customer ID'] = fake.license_plate()
-            dat['Segment'] = choice(['A', 'B', 'C', 'D'])
-            dat['Country'] = 'US'
-            dat['City'] = fake.city()
-            dat['State'] = fake.state()
-            dat['Postal Code'] = fake.zipcode()
-            dat['Region'] = choice(['Region %d' % i for i in range(5)])
-            dat['Product ID'] = fake.bban()
-            sector = choice(['Industrials', 'Technology', 'Financials'])
-            industry = choice(['A', 'B', 'C'])
-            dat['Category'] = sector
-            dat['Sub-Category'] = industry
-            dat['Sales'] = randint(1, 100) * 100
-            dat['Quantity'] = randint(1, 100) * 10
-            dat['Discount'] = round(random() * 100, 2)
-            dat['Profit'] = round(random() * 1000, 2)
-            data.append(dat)
-        return pd.DataFrame(data)
+def superstore(count=10):
+    data = []
+    for id in range(count):
+        dat = {}
+        dat['Row ID'] = id
+        dat['Order ID'] = fake.ein()
+        dat['Order Date'] = fake.date_this_year()
+        dat['Ship Date'] = fake.date_between_dates(dat['Order Date']).strftime('%Y-%m-%d')
+        dat['Order Date'] = dat['Order Date'].strftime('%Y-%m-%d')
+        dat['Ship Mode'] = choice(['First Class', 'Standard Class', 'Second Class'])
+        dat['Ship Mode'] = choice(['First Class', 'Standard Class', 'Second Class'])
+        dat['Customer ID'] = fake.license_plate()
+        dat['Segment'] = choice(['A', 'B', 'C', 'D'])
+        dat['Country'] = 'US'
+        dat['City'] = fake.city()
+        dat['State'] = fake.state()
+        dat['Postal Code'] = fake.zipcode()
+        dat['Region'] = choice(['Region %d' % i for i in range(5)])
+        dat['Product ID'] = fake.bban()
+        sector = choice(['Industrials', 'Technology', 'Financials'])
+        industry = choice(['A', 'B', 'C'])
+        dat['Category'] = sector
+        dat['Sub-Category'] = industry
+        dat['Sales'] = randint(1, 100) * 100
+        dat['Quantity'] = randint(1, 100) * 10
+        dat['Discount'] = round(random() * 100, 2)
+        dat['Profit'] = round(random() * 1000, 2)
+        data.append(dat)
+    return pd.DataFrame(data)
 
-    class TestTableNumpy(object):
-        def test_empty_table(self):
-            tbl = Table([])
-            assert tbl.size() == 0
 
-        def test_table_dataframe(self):
-            import pandas as pd
-            data = pd.DataFrame([{"a": 1, "b": 2}, {"a": 3, "b": 4}])
-            tbl = Table(data)
-            assert tbl.size() == 2
+class TestTableNumpy(object):
+    def test_empty_table(self):
+        tbl = Table([])
+        assert tbl.size() == 0
 
-        def test_table_series(self):
-            import pandas as pd
-            data = pd.Series([1, 2, 3], name="a")
-            tbl = Table(data)
-            assert tbl.size() == 3
+    def test_table_dataframe(self):
+        import pandas as pd
+        data = pd.DataFrame([{"a": 1, "b": 2}, {"a": 3, "b": 4}])
+        tbl = Table(data)
+        assert tbl.size() == 2
 
-        def test_rowpivots(self):
-            # basic
-            df = superstore()
-            df_pivoted = df.set_index(['Country', 'Region'])
-            table = Table(df_pivoted)
+    def test_table_series(self):
+        import pandas as pd
+        data = pd.Series([1, 2, 3], name="a")
+        tbl = Table(data)
+        assert tbl.size() == 3
 
-        def test_pivottable(self):
-            df = superstore()
-            pt = pd.pivot_table(df, values='Discount', index=['Country', 'Region'], columns='Category')
-            table = Table(pt)
+    def test_rowpivots(self):
+        # basic
+        df = superstore()
+        df_pivoted = df.set_index(['Country', 'Region'])
+        table = Table(df_pivoted)
 
-        def test_colpivots(self):
-            arrays = [np.array(['bar', 'bar', 'bar', 'bar', 'baz', 'baz', 'baz', 'baz', 'foo', 'foo', 'foo', 'foo', 'qux', 'qux', 'qux', 'qux']),
-                      np.array(['one', 'one', 'two', 'two', 'one', 'one', 'two', 'two', 'one', 'one', 'two', 'two', 'one', 'one', 'two', 'two']),
-                      np.array(['X', 'Y', 'X', 'Y', 'X', 'Y', 'X', 'Y', 'X', 'Y', 'X', 'Y', 'X', 'Y', 'X', 'Y'])]
-            tuples = list(zip(*arrays))
-            index = pd.MultiIndex.from_tuples(tuples, names=['first', 'second', 'third'])
+    def test_pivottable(self):
+        df = superstore()
+        pt = pd.pivot_table(df, values='Discount', index=['Country', 'Region'], columns='Category')
+        table = Table(pt)
 
-            df_both = pd.DataFrame(np.random.randn(3, 16), index=['A', 'B', 'C'], columns=index)
-            table = Table(df_both)
+    def test_colpivots(self):
+        arrays = [np.array(['bar', 'bar', 'bar', 'bar', 'baz', 'baz', 'baz', 'baz', 'foo', 'foo', 'foo', 'foo', 'qux', 'qux', 'qux', 'qux']),
+                  np.array(['one', 'one', 'two', 'two', 'one', 'one', 'two', 'two', 'one', 'one', 'two', 'two', 'one', 'one', 'two', 'two']),
+                  np.array(['X', 'Y', 'X', 'Y', 'X', 'Y', 'X', 'Y', 'X', 'Y', 'X', 'Y', 'X', 'Y', 'X', 'Y'])]
+        tuples = list(zip(*arrays))
+        index = pd.MultiIndex.from_tuples(tuples, names=['first', 'second', 'third'])
 
-except (ImportError, ModuleNotFoundError):
-    pass
+        df_both = pd.DataFrame(np.random.randn(3, 16), index=['A', 'B', 'C'], columns=index)
+        table = Table(df_both)
