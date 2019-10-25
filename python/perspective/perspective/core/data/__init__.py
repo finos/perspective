35c28546dd7cf4508e6d53405a60500aacbe8160
Tim Paine <timothy.k.paine@jpmorgan.com>
2019-10-25 16:11:10 -0400

    name collision on py2

--- python/perspective/perspective/core/data/__init__.py
+++ python/perspective/perspective/core/data/__init__.py
@@ -6,4 +6,4 @@
 # the Apache License 2.0.  The full license can be found in the LICENSE file.
 #
 
-from .pandas import deconstruct_pandas  # noqa: F401
+from .pd import deconstruct_pandas  # noqa: F401
