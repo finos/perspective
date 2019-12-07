a95f617fac9486930cfcc34bc8f84d0739c1dd42
Andrew Stein <steinlink@gmail.com>
2019-12-07 00:15:42 -0500

    Docs

--- scripts/script_utils.js
+++ scripts/script_utils.js
@@ -1,4 +1,4 @@
-/******************************************************************************
+/*******************************************************************************
  *
  * Copyright (c) 2017, the Perspective Authors.
  *
@@ -13,6 +13,11 @@ const execSync = require("child_process").execSync;
 const _path = require("path");
 const fs = require("fs");
 const rimraf = require("rimraf");
+
+/*******************************************************************************
+ *
+ * Private
+ */
 
 function rimraf_err(e) {
     if (e) {
@@ -33,28 +38,72 @@ function depath(strings, ...args) {
     return strings;
 }
 
+function cut_last(f) {
+    let x = f.split(" ");
+    return x.slice(0, x.length - 1).join(" ");
+}
+
+function cut_first(f) {
+    return f
+        .split(" ")
+        .slice(1)
+        .join(" ");
+}
+
+const execute = cmd => {
+    try {
+        if (process.argv.indexOf("--debug") > -1) {
+            console.log(`$ ${cmd}`);
+        }
+        execSync(cmd, {stdio: "inherit"});
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
+};
+
+/*******************************************************************************
+ *
+ * Public
+ */
+
+/**
+ * Calls `path.join` on the result of splitting the input string by the default
+ * path delimiter `/`, which allows writing simpler path statements that will
+ * still be cross platform.  Can be used as an template literal.
+ *
+ * @param {string} path a `/` encoded path.
+ * @returns {string} A system-correct path
+ * @example
+ * console.assert(path`a/b/c` === `a\\b\\c`) // on Windows
+ */
 exports.path = function path(strings, ...args) {
     return _path.join(...depath(strings, ...args));
 };
 
+/**
+ * Like `path`, but uses `path.resolve` to get the absolute path, carefully
+ * preserving leading delimiter.  Can be used as an template literal.
+ *
+ * @param {string} path a relative `/` encoded path.
+ * @returns {string} A system-correct absolute path
+ * @example
+ * console.assert(path`a/b/c` === `${process.cwd()}\\a\\b\\v`) // on Windows
+ */
 const resolve = (exports.resolve = function resolve(strings, ...args) {
     return _path.resolve(...depath(strings, ...args));
 });
 
-const PATH_TESTS = [
-    [resolve`a/b/c`, `${process.cwd()}/a/b/c`],
-    [resolve`${__dirname}/../cpp/perspective`, `${process.cwd()}/cpp/perspective`],
-    [resolve`${__dirname}/../python/perspective/dist`, _path.resolve(__dirname, "..", "python", "perspective", "dist")],
-    [resolve`${__dirname}/../cpp/perspective`, _path.resolve(__dirname, "..", "cpp", "perspective")],
-    [resolve`${__dirname}/../cmake`, _path.resolve(__dirname, "..", "cmake")],
-    [resolve`${resolve`${__dirname}/../python/perspective/dist`}/cmake`, _path.resolve(_path.resolve(__dirname, "..", "python", "perspective", "dist"), "cmake")],
-    [resolve`${resolve`${__dirname}/../python/perspective/dist`}/obj`, _path.resolve(_path.resolve(__dirname, "..", "python", "perspective", "dist"), "obj")]
-];
-
-for (const [actual, expected] of PATH_TESTS) {
-    console.assert(actual === expected, `"${actual}" received, expected: "${expected}"`);
-}
-
+/**
+ * Calls `join` on each of the input path arguments, then `rimraf`s the path if
+ * it exists.   Can be used as an template literal, and can also take multiple
+ * arguments call itself in sequence.
+ *
+ * @param {string} [path] a `/` encode path.
+ * @example
+ * clean`a/b/c`; // Cleans this dir
+ * clean(path`a/b/c`, path`d/e/f`); // Cleans both dirs
+ */
 exports.clean = function clean(...dirs) {
     if (Array.isArray(dirs[0])) {
         const dir = exports.path(...dirs);
@@ -71,18 +120,19 @@ exports.clean = function clean(...dirs) {
     }
 };
 
-function cut_last(f) {
-    let x = f.split(" ");
-    return x.slice(0, x.length - 1).join(" ");
-}
-
-function cut_first(f) {
-    return f
-        .split(" ")
-        .slice(1)
-        .join(" ");
-}
-
+/**
+ * For working with shell commands, `bash` knows how to remove consecutive
+ * text from strings when arguments are "falsey", which makes mapping flags to
+ * JS expressions a breeze.  Can be used as a template literal.
+ *
+ * @param {string} expression a bash command to be templated.
+ * @returns {string} A command with the missing argument's flags removed.
+ * @example
+ * console.assert(
+ *     bash`run -t${1} -u"${undefined}" task`,
+ *    `run -t1 task`
+ * );
+ */
 const bash = (exports.bash = function bash(strings, ...args) {
     let terms = [];
     if (strings.length === 1) {
@@ -105,21 +155,97 @@ const bash = (exports.bash = function bash(strings, ...args) {
         .trim();
 });
 
-const execute = cmd => {
-    try {
-        if (process.argv.indexOf("--debug") > -1) {
-            console.log(`$ ${cmd}`);
-        }
-        execSync(cmd, {stdio: "inherit"});
-    } catch (e) {
-        console.error(e);
-        process.exit(1);
-    }
-};
-
+/**
+ * Just like `bash, but executes the command immediately.  Will log if the
+ * `--debug` flag is used to build.
+ *
+ * @param {string} expression a bash command to be templated.
+ * @returns {string} A command with the missing argument's flags removed.
+ * @example
+ * execute`run -t${1} -u"${undefined}" task`;
+ */
 exports.execute = (strings, ...args) => execute(Array.isArray(strings) ? bash(strings, ...args) : strings);
 
-const TESTS = [
+/**
+ * Returns the value after this command-line flag, or `true` if it is the last
+ * arg.  This makes it easy to null-pun for boolean flags, and capture the
+ * argument for argument-providing flags, and respect quotes and parens, in
+ * one function.  Can be used as a template literal - not sure why, 2 less
+ * characters?
+ *
+ * @param {string} flag The command line flag name.  Returns all arguments if
+ *     this param is `undefined`.
+ * @returns {string} The next argument after this flag in the command args, or
+ *     `true.
+ * @example
+ * console.assert(getarg`--debug`);
+ */
+const getarg = (exports.getarg = function(flag, ...args) {
+    if (Array.isArray(flag)) {
+        flag = flag.map((x, i) => x + (args[i] || "")).join("");
+    }
+    const argv = process.argv.slice(2);
+    if (flag) {
+        const index = argv.indexOf(flag);
+        if (index > -1) {
+            const next = argv[index + 1];
+            if (next) {
+                return next;
+            } else {
+                return true;
+            }
+        }
+    } else {
+        return argv
+            .map(function(arg) {
+                return "'" + arg.replace(/'/g, "'\\''") + "'";
+            })
+            .join(" ");
+    }
+});
+
+/**
+ * A `bash` expression for running commands in Docker images
+ *
+ * @param {string} image The Docker image name.
+ * @returns {string} A command for invoking this docker image.
+ * @example
+ * execute`${docker()} echo "Hello from Docker"`;
+ */
+exports.docker = function docker(image = "puppeteer") {
+    console.log(`-- Creating perspective/${image} docker image`);
+    const IS_WRITE = getarg("--write") || process.env.WRITE_TESTS;
+    const CPUS = parseInt(process.env.PSP_CPU_COUNT);
+    const PACKAGE = process.env.PACKAGE;
+    const CWD = process.cwd();
+    return bash`docker run -it --rm -eWRITE_TESTS=${IS_WRITE} \
+        -ePACKAGE="${PACKAGE}" -v${CWD}:/usr/src/app/perspective \
+        -w /usr/src/app/perspective --shm-size=2g -u root \
+        --cpus="${CPUS}.0" perspective/${image}`;
+};
+
+/*******************************************************************************
+ *
+ * Tests
+ */
+
+function run_suite(tests) {
+    for (const [actual, expected] of tests) {
+        console.assert(actual === expected, `"${actual}" received, expected: "${expected}"`);
+    }
+}
+
+run_suite([
+    [resolve`a/b/c`, `${process.cwd()}/a/b/c`],
+    [resolve`${__dirname}/../cpp/perspective`, `${process.cwd()}/cpp/perspective`],
+    [resolve`${__dirname}/../python/perspective/dist`, _path.resolve(__dirname, "..", "python", "perspective", "dist")],
+    [resolve`${__dirname}/../cpp/perspective`, _path.resolve(__dirname, "..", "cpp", "perspective")],
+    [resolve`${__dirname}/../cmake`, _path.resolve(__dirname, "..", "cmake")],
+    [resolve`${resolve`${__dirname}/../python/perspective/dist`}/cmake`, _path.resolve(_path.resolve(__dirname, "..", "python", "perspective", "dist"), "cmake")],
+    [resolve`${resolve`${__dirname}/../python/perspective/dist`}/obj`, _path.resolve(_path.resolve(__dirname, "..", "python", "perspective", "dist"), "obj")]
+]);
+
+run_suite([
     [bash`run -t${1}`, `run -t1`],
     [bash`run -t${undefined}`, `run`],
     [bash`run -t${true}`, `run -ttrue`],
@@ -143,32 +269,4 @@ const TESTS = [
     [bash`TEST=${undefined}`, ``],
     [bash`this is a test`, `this is a test`],
     [bash`this is a test `, `this is a test `]
-];
-
-for (const [actual, expected] of TESTS) {
-    console.assert(actual === expected, `"${actual}" received, expected: "${expected}"`);
-}
-
-const getarg = (exports.getarg = function(flag) {
-    const args = process.argv.slice(2);
-    if (flag) {
-        const index = args.indexOf(flag);
-        if (index > -1) {
-            return process.argv.slice(2)[index + 1] || true;
-        }
-    } else {
-        return args;
-    }
-});
-
-exports.docker = function docker(image = "puppeteer") {
-    console.log(`-- Creating perspective/${image} docker image`);
-    const IS_WRITE = getarg("--write") || process.env.WRITE_TESTS;
-    const CPUS = parseInt(process.env.PSP_CPU_COUNT);
-    const PACKAGE = process.env.PACKAGE;
-    const CWD = process.cwd();
-    return bash`docker run -it --rm -eWRITE_TESTS=${IS_WRITE} \
-        -ePACKAGE="${PACKAGE}" -v${CWD}:/usr/src/app/perspective \
-        -w /usr/src/app/perspective --shm-size=2g -u root \
-        --cpus="${CPUS}.0" perspective/${image}`;
-};
+]);
