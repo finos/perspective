8c22820d84a47d60bf95861ad5b040fa918f494a
Andrew Stein <steinlink@gmail.com>
2020-01-07 19:36:12 -0500

    v0.4.0 version fix

--- docs/logo.config.js
+++ docs/logo.config.js
@@ -1,11 +1,14 @@
 const path = require("path");
 const common = require("@finos/perspective/src/config/common.config.js");
 
-module.exports = Object.assign({}, common(), {
-    entry: "./logo.js",
-    output: {
-        filename: "logo.js",
-        libraryTarget: "umd",
-        path: path.resolve(__dirname, "static/js")
-    }
+module.exports = common({}, config => {
+    Object.assign(config, {
+        entry: "./logo.js",
+        output: {
+            filename: "logo.js",
+            libraryTarget: "umd",
+            path: path.resolve(__dirname, "static/js")
+        }
+    });
+    return config;
 });
