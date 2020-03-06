a64a69f3f21e14ec864c0db9cb6e9e3cd9b8b174
zemeolotu <zemeolotu@gmail.com>
2020-03-06 18:49:58 -0500

    Add express node.js remote server example

--- examples/datasources/securities.js
+++ examples/datasources/securities.js
@@ -1,17 +1,53 @@
-import perspective from "@finos/perspective";
+/******************************************************************************
+ *
+ * Copyright (c) 2017, the Perspective Authors.
+ *
+ * This file is part of the Perspective library, distributed under the terms of
+ * the Apache License 2.0.  The full license can be found in the LICENSE file.
+ *
+ */
 
-const worker = perspective.shared_worker();
+const perspective = require("@finos/perspective");
+
+const worker = perspective.shared_worker ? perspective.shared_worker() : perspective;
+
+// Cache updates for faster update rates (but less data diversity)>
+const CACHE_INPUT = false;
+
+// If cached, how many updates to cache?
+const CACHE_ENTRIES = 200;
+
+// How many rows per update?
+const UPDATE_SIZE = 50;
+
+// Update every N milliseconds
+const TICK_RATE = 20;
+
+// Size limit of the server-side table
+const TABLE_SIZE = 10000;
 
 const SECURITIES = ["AAPL.N", "AMZN.N", "QQQ.N", "NVDA.N", "TSLA.N", "FB.N", "MSFT.N", "TLT.N", "XIV.N", "YY.N", "CSCO.N", "GOOGL.N", "PCLN.N"];
-
 const CLIENTS = ["Homer", "Marge", "Bart", "Lisa", "Maggie", "Moe", "Lenny", "Carl", "Krusty"];
 
-const newRows = () => {
+const __CACHE__ = [];
+
+perspective.initialize_profile_thread();
+
+/*******************************************************************************
+ *
+ * Slow mode (new rows generated on the fly)
+ */
+
+function choose(choices) {
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function newRows(total_rows) {
     const rows = [];
-    for (let x = 0; x < 5; x++) {
+    for (let x = 0; x < total_rows; x++) {
         rows.push({
-            name: SECURITIES[Math.floor(Math.random() * SECURITIES.length)],
-            client: CLIENTS[Math.floor(Math.random() * CLIENTS.length)],
+            name: choose(SECURITIES),
+            client: choose(CLIENTS),
             lastUpdate: new Date(),
             chg: Math.random() * 20 - 10,
             bid: Math.random() * 10 + 90,
@@ -20,22 +56,61 @@ const newRows = () => {
         });
     }
     return rows;
-};
+}
 
-const getTable = (updating = true) => {
-    const table = worker.table(newRows(), {
-        index: "name"
-    });
+async function init_dynamic({table_size, update_size, tick_rate}) {
+    // Create a `table`.
+    const table = worker.table(newRows(table_size), {limit: table_size});
 
-    if (updating) {
-        const update = () => {
-            table.update(newRows());
-            setTimeout(update, 500);
-        };
-        update();
-    }
+    // The `table` needs to be registered to a name with the Perspective
+    // `WebSocketServer` in order for the client to get a proxy handle to it.
 
+    // Loop and update the `table` oocasionally.
+    (function postRow() {
+        table.update(newRows(update_size));
+        setTimeout(postRow, tick_rate);
+    })();
     return table;
+}
+
+/*******************************************************************************
+ *
+ * Fast mode (rows pre-generated, cached as Arrows)
+ */
+
+async function newArrow(total_rows) {
+    const table = worker.table(newRows(total_rows));
+    const vw = table.view();
+    const arrow = await vw.to_arrow();
+    vw.delete();
+    table.delete();
+    return arrow;
+}
+
+async function populate_cache(cache_entries) {
+    for (let x = 0; x < cache_entries; x++) {
+        let arrow = await newArrow();
+        __CACHE__[x] = arrow;
+    }
+}
+
+async function init_cached({table_size, tick_rate, cache_entries}) {
+    await populate_cache(cache_entries);
+    const table = worker.table(newRows(table_size), {limit: table_size});
+    (function postRow() {
+        const entry = __CACHE__[Math.floor(Math.random() * __CACHE__.length)];
+        table.update(entry);
+        setTimeout(postRow, tick_rate);
+    })();
+    return table;
+}
+
+const getTable = (config = {cached: CACHE_INPUT, tick_rate: TICK_RATE, update_size: UPDATE_SIZE, table_size: TABLE_SIZE, cache_entries: CACHE_ENTRIES}) => {
+    if (config.cached) {
+        return init_cached(config);
+    } else {
+        return init_dynamic(config);
+    }
 };
 
-export const datasource = getTable();
+module.exports = getTable;
---
+++ examples/remote-express/README.md
@@ -0,0 +1,5 @@
+# Remote Example
+
+An express node.js server example hosting a perspective table to a client-side
+perspective running in a WebWorker.  Both "State of the World" and subsequent
+update data are transferred via Arrow-encoded ArrayBuffers over WebSocket.
