3db9298f25c12335f42c005620d26bf2234e67a1
Jun Tan <tanjunyuan@hotmail.co.uk>
2020-03-11 20:05:49 -0400

    fix drag and drop, add expressions readme

--- packages/perspective-viewer/src/js/computed_expression_editor.js
+++ packages/perspective-viewer/src/js/computed_expression_editor.js
@@ -64,6 +64,7 @@ class ComputedExpressionEditor extends HTMLElement {
             try {
                 const parsed = JSON.parse(data);
                 if (Array.isArray(parsed) && parsed.length > 4) {
+                    event.preventDefault();
                     this._expression_input.value += `"${parsed[0]}"`;
                 }
             } catch (e) {
