1b1e8e1d83b6455dd3d0963d60a87655e17759d5
Tim Paine <t.paine154@gmail.com>
2020-03-22 11:05:19 -0400

    reenable tbb

--- cpp/perspective/src/include/perspective/first.h
+++ cpp/perspective/src/include/perspective/first.h
@@ -7,6 +7,12 @@
  *
  */
 
+#ifndef PSP_ENABLE_WASM
+#ifndef PSP_PARALLEL_FOR
+#define PSP_PARALLEL_FOR
+#endif
+#endif
+
 #if !defined(__linux__) && !defined(__APPLE__) && !defined(WIN32)
 // default to linux
 #define __linux__
