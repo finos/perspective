103439e94ef58381c8efeff42b8481f1c7901b9a
Tim Paine <t.paine154@gmail.com>
2020-05-24 15:55:25 -0400

    typo, no @ sign on disk

--- packages/perspective-jupyterlab/src/ts/extension.ts
+++ packages/perspective-jupyterlab/src/ts/extension.ts
@@ -15,6 +15,6 @@
 // url for the notebook is not known at build time and is therefore computed
 // dynamically.
 // eslint-disable-next-line @typescript-eslint/camelcase
-(window as any).__webpack_public_path__ = document.querySelector("body")!.getAttribute("data-base-url") + "nbextensions/@finos/perspective-jupyterlab";
+(window as any).__webpack_public_path__ = document.querySelector("body")!.getAttribute("data-base-url") + "nbextensions/finos-perspective-jupyterlab";
 
 export * from "./index";
