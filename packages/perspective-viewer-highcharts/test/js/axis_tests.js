66c751c99ac69898969049e164e63bb368e877dd
Jun Tan <tanjunyuan@hotmail.co.uk>
2020-07-14 13:05:14 -0400

    Fix tests for highcharts

--- packages/perspective-viewer-highcharts/test/js/axis_tests.js
+++ packages/perspective-viewer-highcharts/test/js/axis_tests.js
@@ -27,14 +27,12 @@ exports.default = function() {
             await page.waitForSelector("perspective-viewer:not([updating])");
         });
 
-        test.capture("Sets a category axis when pivoted by a computed datetime", async page => {
+        test.capture("Sets a category axis when category is a computed datetime", async page => {
             const viewer = await page.$("perspective-viewer");
             await page.shadow_click("perspective-viewer", "#config_button");
             await page.evaluate(element => element.setAttribute("computed-columns", JSON.stringify(["hour_bucket('Ship Date')"])), viewer);
             await page.waitForSelector("perspective-viewer:not([updating])");
-            await page.evaluate(element => element.setAttribute("row-pivots", '["hour_bucket(Ship Date)"]'), viewer);
-            await page.waitForSelector("perspective-viewer:not([updating])");
-            await page.evaluate(element => element.setAttribute("columns", '["State","Sales"]'), viewer);
+            await page.evaluate(element => element.setAttribute("columns", '["hour_bucket(Ship Date)","Sales"]'), viewer);
             await page.waitForSelector("perspective-viewer:not([updating])");
             await page.evaluate(element => element.setAttribute("aggregates", '{"State":"dominant","Sales":"sum"}'), viewer);
             await page.waitForSelector("perspective-viewer:not([updating])");
