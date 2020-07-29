1c32a5e4d5de3d637d7b365b7c53132773c19ef4
Jun Tan <tanjunyuan@hotmail.co.uk>
2020-07-28 22:07:18 -0400

    Remove hypergrid and highcharts in PerspectiveWidget, replace with datagrid and d3fc

--- packages/perspective-jupyterlab/src/config/webpack.config.js
+++ packages/perspective-jupyterlab/src/config/webpack.config.js
@@ -28,9 +28,20 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.less$/,
+                use: [{loader: "style-loader"}, {loader: "css-loader"}, {loader: "less-loader"}]
+            },
+            {
                 test: /\.css$/,
                 exclude: /node_modules/,
                 use: [{loader: "css-loader"}]
+            },
+            {
+                test: /\.(html)$/,
+                use: {
+                    loader: "html-loader",
+                    options: {}
+                }
             },
             {
                 test: /\.ts$/,
