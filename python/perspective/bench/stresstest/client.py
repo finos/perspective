11991d0991e8c5e023187def647e49550182a7ea
Jun Tan <tanjunyuan@hotmail.co.uk>
2020-08-07 14:40:41 -0400

    Add wait times, randomize methods

--- python/perspective/bench/stresstest/client.py
+++ python/perspective/bench/stresstest/client.py
@@ -5,10 +5,14 @@
 # This file is part of the Perspective library, distributed under the terms of
 # the Apache License 2.0.  The full license can be found in the LICENSE file.
 #
+import six
+import sys
 import random
 import logging
 import json
-from tornado import gen, ioloop, locks, websocket
+
+from datetime import datetime, timedelta
+from tornado import gen, ioloop, websocket
 
 
 class PerspectiveWebSocketClient(object):
@@ -16,6 +20,9 @@ class PerspectiveWebSocketClient(object):
     def __init__(self, url):
         """Create a PerspectiveWebSocketClient that mimics a Perspective Viewer
         through the wire API.
+
+        Args:
+            url (:obj:`str`)
 
         Examples:
             >>> @gen.coroutine
@@ -30,21 +37,133 @@ class PerspectiveWebSocketClient(object):
         """
         self.url = url
         self.client = None
+
+        # Each message must have a unique integer ID
         self.message_id = -1
-        self.total_message_batches = 15
+
+        # The total number of messages that should be sent to the server on
+        # every call to `self.run()`
+        self.total_messages = 30
         self.cmds = ["table_method", "view_method"]
-        self.table_methods = ["schema", "size", "update"]
+
+        # A list of views created by this client
+        self.views = ["view"]
+
+        # A list of messages that can be decomposed into *args for
+        # self._make_message
+        self.table_methods = ["schema", "size"]
         self.table_methods_mutate = ["update"]
-        self.view_methods = ["schema", "column_paths", "sides", "to_json", "to_columns"]
-        self.view_methods_mutate = ["on_update", "delete"]
+        self.view_methods = ["schema", "computed_schema", "column_paths", "sides", "to_json", "to_columns"]
+
+        # `on_update` callbacks managed by this client
         self.callbacks = {}
+
+        # Metadata about the remote table - schema, typemapped columns, and
+        # column names
+        self.schema = {}
+        self.columns_by_type = {}
+        self.column_names = []
 
     @gen.coroutine
     def connect(self):
         """Create and maintain a websocket client to Perspective, initializing
         the connection with the `init` message."""
         self.client = yield websocket.websocket_connect(self.url)
-        self.write_message({"id": self.message_id, "cmd": "init"})
+        yield self.write_message({"id": self.message_id, "cmd": "init"})
+
+    @gen.coroutine
+    def run(self):
+        """Send random messages at intervals that simulate the actions of a
+        Perspective viewer in the front-end."""
+        for i in range(self.total_messages):
+            message = None
+            cmd = "view_method" if random.random() > 0.5 else "table_method"
+
+            if cmd == "view_method":
+                view_name = random.choice(self.views)
+                method = random.choice(self.view_methods)
+                args = []
+
+                if "to" in method:
+                    args.append({
+                        "start_row": random.randint(0, 500),
+                        "end_row": random.randint(501, 1000)
+                    })
+
+                message = self._make_message(
+                    cmd=cmd,
+                    name=view_name,
+                    method=method,
+                    args=args)
+            else:
+                method = random.choice(self.table_methods)
+                message = self._make_message(
+                    cmd=cmd,
+                    name="table",
+                    method=method)
+
+            logging.info("Sending %s, id: %s", cmd, message["id"])
+            yield self.write_message(message)
+
+            # sort of simulate wait times between user actions
+            if random.random() > 0.75:
+                wait_time = random.random()
+                logging.info("Waiting for %s", wait_time)
+                yield gen.sleep(wait_time)
+
+    @gen.coroutine
+    def run_until_timeout(self, timeout=None):
+        """Run the websocket client until the timeout, which defaults to
+        None and runs forever.
+
+        Keyword Args:
+            timeout (:obj:`float`/:obj:`datetime.timdelta`)
+        """
+        @gen.coroutine
+        def _run(self, timeout):
+            """Internal run callback - always run one iteration, and then
+            run until specified timeout."""
+            if timeout:
+                yield self.run()
+
+                end_time = datetime.now() + timeout
+                logging.info("Running until %s", end_time)
+
+                while datetime.now() <= end_time:
+                    if random.random() > 0.8:
+                        # inject more user actions at random
+                        yield self.run()
+                    yield self.read_message()
+
+                # FIXME timeout does not prematurely exit - it waits for all
+                # yields to complete first.
+                logging.critical("Exiting client: timed out at %s", end_time)
+                sys.exit(0)
+            else:
+                yield self.run()
+
+                logging.info("Running forever")
+                while True:
+                    if random.random() > 0.8:
+                        # inject more user actions at random
+                        yield self.run()
+                    yield self.read_message()
+
+        # Connect server and initialize schema and update callback
+        yield self.connect()
+        yield self.get_table_schema()
+        yield self.register_on_update()
+
+        timeout_delta = None
+
+        if timeout:
+            timeout_delta = None
+            if isinstance(timeout, six.integer_types + (float, )):
+                timeout_delta = timedelta(seconds=timeout)
+            elif isinstance(timeout, timedelta):
+                timeout_delta = timeout
+
+        yield _run(self, timeout_delta)
 
     @gen.coroutine
     def write_message(self, message):
@@ -55,7 +174,10 @@ class PerspectiveWebSocketClient(object):
             json.dumps(message)
         )
         self.message_id += 1
-        yield self.read_message()
+
+        if message["cmd"] != "view":
+            # view creation does not yield a response
+            yield self.read_message()
 
     @gen.coroutine
     def read_message(self):
@@ -63,42 +185,114 @@ class PerspectiveWebSocketClient(object):
         message = yield self.client.read_message()
         try:
             response = self.parse_response(message)
-            print("Received", response["id"])
+            response_id = response["id"]
+            logging.info("Received id: %s", response_id)
 
             if response["id"] in self.callbacks:
                 # Call the on_update callback
-                ioloop.IOLoop.current().add_callback(self.callbacks[response["id"]], self)
+                ioloop.IOLoop.current().add_callback(self.callbacks[response_id], self)
+
+            assert "error" not in response
         except AssertionError:
             logging.CRITICAL("Server returned error:", message)
 
     @gen.coroutine
-    def start(self):
-        """Run a batch of messages to the remote endpoint simulating the
-        actions of a Perspective viewer in the front-end."""
-        for i in range(self.total_message_batches):
-            for method in self.view_methods:
-                args = []
+    def create_view(self):
+        """Create a new view with a randomized config."""
+        view_name = str(random.random())
 
-                if "to" in method:
-                    args.append({
-                        "start_row": random.randint(0, 500),
-                        "end_row": random.randint(501, 1000)
-                    })
+        # 0/1/2 sided, 3 is column only
+        sides = random.randint(0, 3)
 
-                message = self._make_message(
-                    cmd="view_method",
-                    name="view",
-                    method=method,
-                    args=args)
+        column_range = {
+            "start": random.randint(0, len(self.column_names) - 1),
+            "end": random.randint(1, len(self.column_names)) - 1
+        }
 
-                print("sending", message["id"])
-                yield self.write_message(message)
+        config = {
+            "columns": self.column_names[
+                column_range["start"]:column_range["end"]
+            ]
+        }
 
+        pivot_columns = \
+            self.columns_by_type.get("string", []) + \
+            self.columns_by_type.get("datetime", []) + \
+            self.columns_by_type.get("date", [])
+
+        sort_columns = \
+            self.columns_by_type.get("integer", []) + \
+            self.columns_by_type.get("float", [])
+
+        if sides == 1:
+            config["row_pivots"] = [random.choice(pivot_columns) for i in range(random.randint(0, 3))]
+        elif sides == 2:
+            config["row_pivots"] = [random.choice(pivot_columns) for i in range(random.randint(0, 3))]
+            config["column_pivots"] = [random.choice(pivot_columns) for i in range(random.randint(0, 2))]
+        elif sides == 3:
+            config["column_pivots"] = [random.choice(pivot_columns)]
+
+        if random.random() > 0.5:
+            config["sort"] = [[random.choice(sort_columns), "desc"]]
+
+        logging.debug("New view config: %s", config)
+
+        message = self._make_view_message(view_name, config)
+        yield self.write_message(message)
+        self.views.append(view_name)
 
     @gen.coroutine
-    def run_forever(self):
-        while True:
-            yield self.read_message()
+    def delete_view(self, name):
+        """Delete the view on the server with the given name."""
+        if name in self.views and name != "view":
+            message = self.make_message(
+                cmd="view_method",
+                name=name,
+                method="delete"
+            )
+
+            # use the client directly because we don't care about the output
+            ioloop.IOLoop.current().add_callback(
+                self.client.write_message,
+                json.dumps(message)
+            )
+
+            self.message_id += 1
+            self.views.pop(name)
+            logging.debug("Deleted view: %s", name)
+
+    @gen.coroutine
+    def get_table_schema(self):
+        """Get the schema of the table from the server."""
+        message = self._make_message(
+            cmd="table_method",
+            name="table",
+            method="schema"
+        )
+
+        # use the client directly because we care about the output
+        ioloop.IOLoop.current().add_callback(
+            self.client.write_message,
+            json.dumps(message)
+        )
+
+        self.message_id += 1
+
+        response = yield self.client.read_message()
+        parsed = json.loads(response)
+        schema = parsed["data"]
+
+        self.schema = schema
+        self.column_names = schema.keys()
+
+        for name in self.column_names:
+            dtype = self.schema[name]
+            if dtype not in self.columns_by_type:
+                self.columns_by_type[dtype] = []
+            self.columns_by_type[dtype].append(name)
+
+        logging.debug("Table schema: %s", self.schema)
+        logging.debug("Table columns by type: %s", self.columns_by_type)
 
     @gen.coroutine
     def register_on_update(self):
@@ -107,12 +301,20 @@ class PerspectiveWebSocketClient(object):
         thereby calling back into the Tornado IOLoop on the server."""
         @gen.coroutine
         def on_update(self):
-            for method in ("schema", "num_rows", "column_paths"):
+            # Send 5 more messages back to the server whenever on_update
+            # happens, with no sleep time to simulate the Viewer requesting
+            # more information from the server.
+            for i in range(5):
+                # non-mutating methods only
+                method = random.choice(("schema", "num_rows", "column_paths"))
+                view_name = random.choice(self.views)
+
                 message = self._make_message(
                     cmd="view_method",
-                    name="view",
+                    name=view_name,
                     method=method)
-                print("sending_on_update", message["id"])
+
+                logging.info("Sending within on_update %s, id: %s", method, message["id"])
                 yield self.write_message(message)
 
         # Store callbacks as they would be on the viewer - by message ID
@@ -125,8 +327,6 @@ class PerspectiveWebSocketClient(object):
             "on_update",
             subscribe=True,
             callback_id="callback_1")
-
-        print(self.callbacks)
 
         yield self.write_message(on_update_message)
 
@@ -153,7 +353,7 @@ class PerspectiveWebSocketClient(object):
 
         return msg
 
-    def _make_view_message(self, message_id, name, config={}):
+    def _make_view_message(self, name, config={}):
         """Returns a message that will create a view on the server with the
         given `name` and `config`."""
         message = {
--- python/perspective/bench/stresstest/main.py
+++ python/perspective/bench/stresstest/main.py
@@ -5,16 +5,9 @@
 # This file is part of the Perspective library, distributed under the terms of
 # the Apache License 2.0.  The full license can be found in the LICENSE file.
 #
-import os
-import sys
-
-from concurrent.futures import ProcessPoolExecutor
-
+import logging
 import tornado
-from tornado.ioloop import IOLoop
-
 from client import PerspectiveWebSocketClient
-from server import start
 
 
 def get_free_port():
@@ -24,19 +17,12 @@ def get_free_port():
 
 @tornado.gen.coroutine
 def run():
-    port = get_free_port()
-    # server = multiprocessing.Process(target=start, args=(port, ))
-
-    # server.start()
-    # server.join(15)
-
     client = PerspectiveWebSocketClient("ws://127.0.0.1:{}/".format(8888))
-    yield client.connect()
-    yield client.register_on_update()
-    yield client.start()
-    yield client.run_forever()
+    yield client.run_until_timeout(timeout=5)
+
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
     loop = tornado.ioloop.IOLoop.current()
     loop.add_callback(run)
     loop.start()
