c8e33a1df03dcc30e4d416c6bbe5dd0b1878e175
Jun Tan <tanjunyuan@hotmail.co.uk>
2020-08-13 18:32:48 -0400

    Make args clearer in main, add host_results

--- python/perspective/bench/stresstest/main.py
+++ python/perspective/bench/stresstest/main.py
@@ -5,19 +5,34 @@
 # This file is part of the Perspective library, distributed under the terms of
 # the Apache License 2.0.  The full license can be found in the LICENSE file.
 #
-import logging
 import os
+import argparse
+import logging
 import asyncio
 
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 CLIENT_PATH = os.path.join(HERE, "client.py")
 
+PARSER = argparse.ArgumentParser(description="Stress testing for Perspective's websocket interface.")
+
+PARSER.add_argument(
+    "--num_clients",
+    default=5,
+    type=int,
+    dest="num_clients",
+    help="The number of clients to run - each client will have its own subprocess.")
+
 if __name__ == "__main__":
     # Make sure you have an instance of server.py running in a separate
     # Python process. This script will run 10 Python subprocesses, each
     # making websocket requests to the server.
     logging.basicConfig(level=logging.DEBUG)
+
+    args = PARSER.parse_args()
+    NUM_CLIENTS = args.num_clients
+
+    logging.info("Running %d client(s)", NUM_CLIENTS)
 
     results_folder = os.path.join(HERE, "results")
 
@@ -34,7 +49,7 @@ if __name__ == "__main__":
     loop = asyncio.get_event_loop()
 
     tasks = [
-        asyncio.ensure_future(run_subprocess("client_{}".format(i))) for i in range(10)
+        asyncio.ensure_future(run_subprocess("client_{}".format(i))) for i in range(NUM_CLIENTS)
     ]
 
     loop.run_until_complete(asyncio.gather(*tasks))
