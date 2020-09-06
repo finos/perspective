8b01b412e14e125153a89be7e4e13a03db706966
Andrew Stein <steinlink@gmail.com>
2020-09-06 16:09:33 -0400

    Fixed `perspective-viewer-datagrid` sorting to use `perspective-viewer` API directly

---
+++ packages/perspective-viewer-datagrid/src/js/sorting.js
@@ -0,0 +1,15 @@
+/******************************************************************************
+ *
+ * Copyright (c) 2017, the Perspective Authors.
+ *
+ * This file is part of the Perspective library, distributed under the terms of
+ * the Apache License 2.0.  The full license can be found in the LICENSE file.
+ *
+ */
+
+export async function configureSortable(table, viewer) {
+    table.addEventListener("regular-table-psp-sort", event => {
+        this._preserve_focus_state = true;
+        viewer.setAttribute("sort", JSON.stringify(event.detail.sort));
+    });
+}
