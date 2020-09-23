b79f7643c6cdb28278b030d7534fdc7b4d6ecfc3
Jun Tan <tanjunyuan@hotmail.co.uk>
2020-09-23 13:34:54 -0400

    Single start command for workspace-editing-python

--- examples/workspace-editing-python/src/server.py
+++ examples/workspace-editing-python/src/server.py
@@ -1,9 +1,17 @@
+################################################################################
+#
+# Copyright (c) 2019, the Perspective Authors.
+#
+# This file is part of the Perspective library, distributed under the terms of
+# the Apache License 2.0.  The full license can be found in the LICENSE file.
+#
 import os
 import os.path
 import logging
 import tornado.websocket
 import tornado.web
 import tornado.ioloop
+import threading
 
 from perspective import Table, PerspectiveManager, PerspectiveTornadoHandler
 
@@ -20,29 +28,54 @@ file_path = os.path.join(
 )
 
 
-def make_app():
+def perspective_thread(manager):
+    """Perspective application thread starts its own tornado IOLoop, and
+    adds the table with the name "data_source_one", which will be used
+    in the front-end."""
+    psp_loop = tornado.ioloop.IOLoop()
+
+    # Set Perspective to run in async mode
+    manager.set_loop_callback(psp_loop.add_callback)
+
     with open(file_path, mode="rb") as file:
-        # Create an instance of `PerspectiveManager` and a table.
-        MANAGER = PerspectiveManager()
-        TABLE = Table(file.read(), index="Row ID")
+        table = Table(file.read(), index="Row ID")
+        manager.host_table("data_source_one", table)
+        manager.host_view("view_one", table.view())
 
-        # Track the table with the name "data_source_one", which will be used
-        # in the front-end to access the Table.
-        MANAGER.host_table("data_source_one", TABLE)
-        MANAGER.host_view("view_one", TABLE.view())
+    psp_loop.start()
 
-        return tornado.web.Application(
-            [
-                # create a websocket endpoint that the client Javascript
-                # can access
-                (
-                    r"/websocket",
-                    PerspectiveTornadoHandler,
-                    {"manager": MANAGER, "check_origin": True},
-                )
-            ],
-            websocket_ping_interval=15,
-        )
+
+def make_app():
+    manager = PerspectiveManager()
+
+    # Run Perspective in its own thread with its own IOLoop
+    thread = threading.Thread(target=perspective_thread, args=(manager,))
+    thread.daemon = True
+    thread.start()
+
+    return tornado.web.Application(
+        [
+            (
+                r"/websocket",
+                PerspectiveTornadoHandler,
+                {"manager": manager, "check_origin": True},
+            ),
+            (
+                r"/node_modules/(.*)",
+                tornado.web.StaticFileHandler,
+                {"path": "../../../node_modules/@finos/"},
+            ),
+            (
+                r"/(.*)",
+                tornado.web.StaticFileHandler,
+                {
+                    "path": os.path.join(here, "..", "dist"),
+                    "default_filename": "index.html",
+                },
+            ),
+        ],
+        websocket_ping_interval=15,
+    )
 
 
 if __name__ == "__main__":
