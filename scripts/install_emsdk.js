062dc9d7a1b278ca4ceb0a83a5f74d7f0789f83c
Andrew Stein <steinlink@gmail.com>
2021-03-04 04:35:37 -0500

    Replace `emsdk-npm` with simple script

--- scripts/install_emsdk.js
+++ scripts/install_emsdk.js
@@ -9,23 +9,45 @@
 
 require("dotenv").config({path: "./.perspectiverc"});
 
-const {execute, execute_throw} = require("./script_utils.js");
+const {execute_throw} = require("./script_utils.js");
 const {emscripten} = require("../package.json");
+const path = require("path");
+const os = require("os");
+const fs = require("fs");
+
+function base() {
+    return path.join(__dirname, "..", ".emsdk");
+}
+
+function emsdk_checkout() {
+    function git(args) {
+        execute_throw`git ${args.join(" ")}`;
+    }
+
+    git(["clone", "https://github.com/emscripten-core/emsdk.git", base()]);
+}
+
+function emsdk(...args) {
+    const basedir = base();
+    const suffix = os.type() == "Windows_NT" ? ".bat" : "";
+    const emsdk = path.join(basedir, "emsdk" + suffix);
+    execute_throw`${emsdk} ${args.join(" ")}`;
+}
 
 function upgrade() {
     console.log(`-- Emscripten not found, installing ${emscripten}`);
-    execute`yarn emsdk-checkout`;
-    execute`yarn emsdk install ${emscripten}`;
-    execute`yarn emsdk activate ${emscripten}`;
+    emsdk_checkout();
+    emsdk("install", emscripten);
+    emsdk("activate", emscripten);
     console.log(`-- Emscripten ${emscripten} installed`);
 }
 
 function check() {
     try {
-        execute_throw`yarn emsdk-run command -v emcc`;
+        execute_throw`emcc --version`;
         return true;
     } catch (e) {
-        return false;
+        return fs.existsSync(path.join(__dirname, "..", ".emsdk"));
     }
 }
 
---
+++ scripts/run_emsdk.js
@@ -0,0 +1,21 @@
+/******************************************************************************
+ *
+ * Copyright (c) 2017, the Perspective Authors.
+ *
+ * This file is part of the Perspective library, distributed under the terms of
+ * the Apache License 2.0.  The full license can be found in the LICENSE file.
+ *
+ */
+
+const {execute_throw} = require("./script_utils.js");
+const path = require("path");
+
+try {
+    const cwd = process.cwd();
+    const cmd = process.argv.slice(2).join(" ");
+    const emsdkdir = path.join(__dirname, "..", ".emsdk");
+    execute_throw`cd ${emsdkdir} && . ./emsdk_env.sh && cd ${cwd} && ${cmd}`;
+} catch (e) {
+    console.log(e.message);
+    process.exit(1);
+}
