72a8c7db50e256d3cd316b021bb80e6652a2e5de
Andrew Stein <steinlink@gmail.com>
2021-11-17 14:19:55 -0600

    Add wheel download and split js & python publish

--- scripts/publish_js.js
+++ scripts/publish_js.js
@@ -7,8 +7,20 @@
  *
  */
 
-const {execute, clean} = require("./script_utils.js");
+const {execute} = require("./script_utils.js");
 const fs = require("fs");
+
+if (!process.env.AZURE_TOKEN) {
+    throw new Error("Missing AZURE_TOKEN");
+}
+
+if (!process.env.AZURE_BUILD_ID) {
+    throw new Error("Missing AZURE_BUILD_ID");
+}
+
+if (!process.env.GITHUB_TOKEN) {
+    throw new Error("Missing GITHUB_TOKEN");
+}
 
 try {
     // NPM publish
@@ -27,7 +39,7 @@ try {
 
     execute`git add CHANGELOG.md`;
 
-    console.log(`-- Building "@finos/perspective(-*)" ${PERSPECTIVE_VERSION}`);
+    console.log(`-- Building "@finos/perspective(-*)"`);
     fs.writeFileSync("./.perspectiverc", `PSP_PROJECT=js`);
     execute`rm -rf node_modules`;
     execute`yarn clean --deps`;
@@ -35,23 +47,6 @@ try {
     execute`yarn build`;
 
     execute`yarn lerna publish --force-publish --no-push`;
-
-    // publish is run after version, so any package.json has the right version
-    const pkg_json = require("@finos/perspective/package.json");
-    const PERSPECTIVE_VERSION = pkg_json.version;
-
-    // Python publish
-    console.log(`-- Building "perspective-python" ${PERSPECTIVE_VERSION}`);
-    fs.writeFileSync("./.perspectiverc", `PSP_PROJECT=python`);
-    execute`yarn clean --deps`;
-    execute`yarn build`;
-
-    // sdist into `python/perspective/dist`, and test the sdist as well.
-    execute`cd ./python/perspective && ./scripts/build_sdist.sh`;
-    const sdist_name = `perspective-python-${PERSPECTIVE_VERSION}.tar.gz`;
-
-    console.log(`-- Uploading source distribution "${sdist_name}" to PyPi"`);
-    execute`cd python/perspective && python3 -m twine upload ./dist/${sdist_name}`;
 } catch (e) {
     console.error(e.message);
     process.exit(1);
