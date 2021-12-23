ee36cf96210cc4229e28142fae1ffbb10b199fe6
Andrew Stein <steinlink@gmail.com>
2021-12-22 22:42:37 -0600

    Add other datetime format parse options (for chrome)

--- rust/perspective-viewer/src/rust/utils/datetime.rs
+++ rust/perspective-viewer/src/rust/utils/datetime.rs
@@ -11,11 +11,21 @@ use crate::utils::*;
 use chrono::{DateTime, FixedOffset, NaiveDateTime, TimeZone, Utc};
 use wasm_bindgen::prelude::*;
 
-pub fn posix_to_utc_str(x: f64) -> Result<String, JsValue> {
-    let tz = FixedOffset::west(
-        js_sys::Date::new(&0.into()).get_timezone_offset() as i32 * 60,
-    );
+fn input_value_format(x: &str) -> Result<&str, JsValue> {
+    match x.len() {
+        23 => Ok("%Y-%m-%dT%H:%M:%S%.3f"),
+        19 => Ok("%Y-%m-%dT%H:%M:%S"),
+        16 => Ok("%Y-%m-%dT%H:%M"),
+        _ => Err(format!("Unknown format {}", x).into()),
+    }
+}
 
+fn get_local_tz() -> FixedOffset {
+    FixedOffset::west(js_sys::Date::new(&0.into()).get_timezone_offset() as i32 * 60)
+}
+
+pub fn posix_to_utc_str(x: f64) -> Result<String, JsValue> {
+    let tz = get_local_tz();
     if x > 0_f64 {
         Ok(Utc
             .timestamp(x as i64 / 1000, ((x as i64 % 1000) * 1000000) as u32)
@@ -28,11 +38,8 @@ pub fn posix_to_utc_str(x: f64) -> Result<String, JsValue> {
 }
 
 pub fn str_to_utc_posix(val: &str) -> Result<f64, JsValue> {
-    let tz = FixedOffset::west(
-        js_sys::Date::new(&0.into()).get_timezone_offset() as i32 * 60,
-    );
-
-    NaiveDateTime::parse_from_str(val, "%Y-%m-%dT%H:%M:%S%.3f")
+    let tz = get_local_tz();
+    NaiveDateTime::parse_from_str(val, input_value_format(val)?)
         .map(|ref posix| {
             DateTime::<Utc>::from(tz.from_local_datetime(posix).unwrap())
                 .timestamp_millis() as f64
