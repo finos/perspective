9417b135dd4f84565c30ba0dc7913d185eb1d19d
Andrew Stein <steinlink@gmail.com>
2022-03-09 19:45:20 -0500

    Add adaptive stroke-width for line charts

--- packages/perspective-viewer-d3fc/src/js/series/lineSeries.js
+++ packages/perspective-viewer-d3fc/src/js/series/lineSeries.js
@@ -12,10 +12,21 @@ import {withoutOpacity} from "./seriesColors.js";
 export function lineSeries(settings, color) {
     let series = fc.seriesSvgLine();
 
+    const estimated_size =
+        settings.data.length *
+        (settings.data?.length > 0
+            ? Object.keys(settings.data[0]).length -
+              (settings.crossValues?.length > 0 ? 1 : 0)
+            : 0);
+    const stroke_width = Math.max(
+        1,
+        Math.min(3, Math.floor(settings.size.width / estimated_size / 2))
+    );
+
     series = series.decorate((selection) => {
-        selection.style("stroke", (d) =>
-            withoutOpacity(color(d[0] && d[0].key))
-        );
+        selection
+            .style("stroke", (d) => withoutOpacity(color(d[0] && d[0].key)))
+            .style("stroke-width", stroke_width);
     });
 
     return series.crossValue((d) => d.crossValue).mainValue((d) => d.mainValue);
