1ec65f0388e1ce3f8b6f7e67f5142613a0cc5af0
Andrew Stein <steinlink@gmail.com>
2022-03-11 13:55:03 -0500

    Fix flatbuffers

--- cmake/modules/FindFlatbuffers.cmake
+++ cmake/modules/FindFlatbuffers.cmake
@@ -35,29 +35,30 @@ cmake_minimum_required(VERSION 3.7.2)
 
 # this might fail
 # https://gitlab.kitware.com/cmake/cmake/issues/19120
-find_path(FLATBUFFERS_INCLUDE_DIR flatbuffers/flatbuffers.h
-  PATHS ${FLATBUFFERS_ROOT}/include
-  HINTS /usr/local /usr/local/flatbuffers /usr/local/Homebrew /usr ~/homebrew/ /usr/local/include /usr/local/flatbuffers/include /usr/include ~/homebrew/include /opt/homebrew/include  ${CMAKE_SOURCE_DIR}/../../vcpkg/installed/x64-windows/include
-  NO_CMAKE_SYSTEM_PATH
-  NO_SYSTEM_ENVIRONMENT_PATH)
+#
+# find_path(FLATBUFFERS_INCLUDE_DIR flatbuffers/flatbuffers.h
+#   PATHS ${FLATBUFFERS_ROOT}/include
+#   HINTS /usr/local /usr/local/flatbuffers /usr/local/Homebrew /usr ~/homebrew/ /usr/local/include /usr/local/flatbuffers/include /usr/include ~/homebrew/include /opt/homebrew/include  ${CMAKE_SOURCE_DIR}/../../vcpkg/installed/x64-windows/include
+#   NO_CMAKE_SYSTEM_PATH
+#   NO_SYSTEM_ENVIRONMENT_PATH)
 
-find_program(FLATBUFFERS_COMPILER flatc
-  PATHS ${FLATBUFFERS_ROOT}/bin
-  HINTS /usr/local/bin /usr/bin /usr/local/Homebrew/bin ~/homebrew/bin /opt/homebrew/bin ${CMAKE_SOURCE_DIR}/../../vcpkg/installed/x64-windows/tools/flatbuffers
-  NO_CMAKE_SYSTEM_PATH
-  NO_SYSTEM_ENVIRONMENT_PATH)
+# find_program(FLATBUFFERS_COMPILER flatc
+#   PATHS ${FLATBUFFERS_ROOT}/bin
+#   HINTS /usr/local/bin /usr/bin /usr/local/Homebrew/bin ~/homebrew/bin /opt/homebrew/bin ${CMAKE_SOURCE_DIR}/../../vcpkg/installed/x64-windows/tools/flatbuffers
+#   NO_CMAKE_SYSTEM_PATH
+#   NO_SYSTEM_ENVIRONMENT_PATH)
 
-if(NOT ${FLATBUFFERS_INCLUDE_DIR})
-  # HACK
-  set(FLATBUFFERS_INCLUDE_DIR /usr/local/include)
-endif()
+# if(NOT ${FLATBUFFERS_INCLUDE_DIR})
+#   # HACK
+#   set(FLATBUFFERS_INCLUDE_DIR /usr/local/include)
+# endif()
 
-include(FindPackageHandleStandardArgs)
+# include(FindPackageHandleStandardArgs)
 
-if (WIN32)
-  find_package_handle_standard_args(Flatbuffers REQUIRED_VARS
-    FLATBUFFERS_INCLUDE_DIR FLATBUFFERS_COMPILER)
-else()
-  find_package_handle_standard_args(FLATBUFFERS REQUIRED_VARS
-    FLATBUFFERS_INCLUDE_DIR FLATBUFFERS_COMPILER)
-endif()
+# if (WIN32)
+#   find_package_handle_standard_args(Flatbuffers REQUIRED_VARS
+#     FLATBUFFERS_INCLUDE_DIR FLATBUFFERS_COMPILER)
+# else()
+#   find_package_handle_standard_args(FLATBUFFERS REQUIRED_VARS
+#     FLATBUFFERS_INCLUDE_DIR FLATBUFFERS_COMPILER)
+# endif()
