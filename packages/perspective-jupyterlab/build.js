76b9b23524f91a69abc543e691f73c9da77e0bd8
Andrew Stein <steinlink@gmail.com>
2022-05-30 22:38:49 -0400

    Remove webpack

--- packages/perspective-jupyterlab/build.js
+++ packages/perspective-jupyterlab/build.js
@@ -1,9 +1,29 @@
 const cpy_mod = import("cpy");
-
+const { lessLoader } = require("esbuild-plugin-less");
 const { WasmPlugin } = require("@finos/perspective-esbuild-plugin/wasm");
 const { WorkerPlugin } = require("@finos/perspective-esbuild-plugin/worker");
+const { AMDLoader } = require("@finos/perspective-esbuild-plugin/amd");
 const { UMDLoader } = require("@finos/perspective-esbuild-plugin/umd");
+const { ReplacePlugin } = require("@finos/perspective-esbuild-plugin/replace");
 const { build } = require("@finos/perspective-esbuild-plugin/build");
+const path = require("path");
+
+const NBEXTENSION_PATH = path.resolve(
+    __dirname,
+    "..",
+    "..",
+    "python",
+    "perspective",
+    "perspective",
+    "nbextension",
+    "static"
+);
+
+const THEMES_BUILD = {
+    entryPoints: ["src/less/index.less"],
+    plugins: [WasmPlugin(false), lessLoader()],
+    outdir: "dist/css",
+};
 
 const TEST_BUILD = {
     entryPoints: ["src/js/psp_widget.js"],
@@ -21,12 +41,19 @@ const TEST_BUILD = {
     outfile: "dist/umd/lumino.js",
 };
 
-const PROD_BUILD = {
+const LAB_BUILD = {
     entryPoints: ["src/js/index.js"],
     define: {
         global: "window",
     },
-    plugins: [WasmPlugin(true), WorkerPlugin({ inline: true })],
+    plugins: [
+        lessLoader(),
+        WasmPlugin(true), 
+        WorkerPlugin({ inline: true }),
+
+        // See note in `rust/perspective-viewer/build.js`
+        ReplacePlugin(/["']perspective_viewer_bg\.wasm["']/, "undefined"),
+    ],
     external: ["@jupyter*", "@lumino*"],
     format: "esm",
     loader: {
@@ -37,8 +64,38 @@ const PROD_BUILD = {
     outfile: "dist/umd/perspective-jupyterlab.js",
 };
 
-const BUILD = [
-    process.argv.some((x) => x == "--test") ? TEST_BUILD : PROD_BUILD,
+const NB_BUILDS = [
+    {
+        entryPoints: ["src/js/notebook/extension.js"],
+        define: {
+            global: "window",
+        },
+        plugins: [WasmPlugin(true), WorkerPlugin(true), AMDLoader([])],
+        loader: {
+            ".ttf": "file",
+            ".css": "text",
+        },
+        external: ["@jupyter*", "@lumino*"],
+        format: "cjs",
+        outfile: path.join(NBEXTENSION_PATH, "extension.js"),
+    },
+    {
+        entryPoints: ["src/js/notebook/index.js"],
+        define: {
+            global: "window",
+        },
+        plugins: [
+            WasmPlugin(true),
+            WorkerPlugin(true),
+            AMDLoader([`@jupyter-widgets/base`]),
+        ],
+        external: ["@jupyter*"],
+        format: "cjs",
+        loader: {
+            ".ttf": "file",
+        },
+        outfile: path.join(NBEXTENSION_PATH, "index.js"),
+    },
 ];
 
 const { BuildCss } = require("@prospective.co/procss/target/cjs/procss.js");
@@ -50,6 +107,9 @@ function add(builder, path, path2) {
         fs.readFileSync(require.resolve(path2 || path)).toString()
     );
 }
+
+const PROD_BUILD = [LAB_BUILD, ...NB_BUILDS];
+const BUILD = process.argv.some((x) => x == "--test") ? TEST_BUILD : PROD_BUILD;
 
 async function build_all() {
     const { default: cpy } = await cpy_mod;
@@ -63,6 +123,7 @@ async function build_all() {
         builder3.compile().get("index.css")
     );
 
+    await build(THEMES_BUILD);
     await Promise.all(BUILD.map(build)).catch(() => process.exit(1));
     cpy(["dist/css/*"], "dist/umd");
     cpy(["src/less/*"], "dist/less");
