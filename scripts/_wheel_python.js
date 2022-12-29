705ad52b3bdb2ee774d1bcce78f3f5e3556687d2
Tim Paine <t.paine154@gmail.com>
2022-12-29 17:17:16 -0500

    fix lint for wheel script

--- scripts/_wheel_python.js
+++ scripts/_wheel_python.js
@@ -84,7 +84,9 @@ try {
         // cache and numpy paths from the pep-517/518 build in build_python.js
         cmd += `${PYTHON} setup.py build_ext bdist_wheel `;
         if (IS_ARM) {
-          cmd += `--plat-name=macos_11_0_arm64 `;
+            cmd = `CMAKE_OSX_ARCHITECTURES=arm64 ${cmd} --plat-name=macosx_11_0_arm64 `;
+        } else {
+            cmd = `CMAKE_OSX_ARCHITECTURES=x86_64 ${cmd} `;
         }
         cmd += " && mkdir -p ./wheelhouse && cp -v ./dist/*.whl ./wheelhouse ";
     } else {
