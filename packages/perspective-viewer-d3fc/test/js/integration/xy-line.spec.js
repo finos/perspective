3263809e178b8174bfb63a233056ff6cdb8196d3
Andrew Stein <steinlink@gmail.com>
2023-02-18 23:50:29 -0500

    Fix legend on X/Y line chart

--- packages/perspective-viewer-d3fc/test/js/integration/xy-line.spec.js
+++ packages/perspective-viewer-d3fc/test/js/integration/xy-line.spec.js
@@ -25,6 +25,45 @@ utils.with_server({}, () => {
                     "perspective-viewer perspective-viewer-d3fc-xyline"
                 )
             );
+
+            test.capture(
+                "correctly render when a bar chart has non equidistant times on a datetime axis",
+                async (page) => {
+                    const config = await page.evaluate(async () => {
+                        const viewer =
+                            document.querySelector("perspective-viewer");
+                        await viewer.getTable();
+                        await viewer.restore({
+                            plugin: "X/Y Line",
+                            columns: ["Profit"],
+                            group_by: ["Order Date"],
+                            split_by: ["State"],
+                            filter: [["State", "in", ["NY", "CA"]]],
+                        });
+
+                        return await viewer.save();
+                    });
+
+                    expect(config).toEqual({
+                        plugin: "X/Y Line",
+                        columns: ["Profit"],
+                        group_by: ["Order Date"],
+                        split_by: ["State"],
+                        filter: [["State", "in", ["NY", "CA"]]],
+                        aggregates: {},
+                        sort: [],
+                        plugin_config: {},
+                        settings: false,
+                        expressions: [],
+                        theme: "Material Light",
+                    });
+
+                    return await utils.get_contents.bind(
+                        null,
+                        "perspective-viewer perspective-viewer-d3fc-xyline"
+                    )(page);
+                }
+            );
         },
         { reload_page: false, root: path.join(__dirname, "..", "..", "..") }
     );
