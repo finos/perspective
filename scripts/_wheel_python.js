6a965301eada69090c0d547f9d6a0513b9d64f05
Andrew Stein <steinlink@gmail.com>
2023-05-07 13:06:07 -0400

    Fix windows python builds

--- scripts/_wheel_python.js
+++ scripts/_wheel_python.js
@@ -53,9 +53,9 @@ try {
         // install deps
         const boost = [
             `yum -y install wget libffi-devel`,
-            `wget https://boostorg.jfrog.io/artifactory/main/release/1.71.0/source/boost_1_71_0.tar.gz >/dev/null`,
-            `tar xfz boost_1_71_0.tar.gz`,
-            "cd boost_1_71_0",
+            `wget https://boostorg.jfrog.io/artifactory/main/release/1.81.0/source/boost_1_81_0.tar.gz >/dev/null`,
+            `tar xfz boost_1_81_0.tar.gz`,
+            "cd boost_1_81_0",
             `./bootstrap.sh`,
             `./b2 -j8 --with-program_options --with-filesystem --with-system install`,
             `cd ..`,
