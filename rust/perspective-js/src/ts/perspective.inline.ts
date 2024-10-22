a501c5573d2e4a4836c98d6747b1241b0a524497
Andrew Stein <steinlink@gmail.com>
2024-10-21 20:52:08 -0400

    Fix inline builds
    Signed-off-by: Andrew Stein <steinlink@gmail.com>

--- rust/perspective-js/src/ts/perspective.inline.ts
+++ rust/perspective-js/src/ts/perspective.inline.ts
@@ -18,16 +18,42 @@ import * as wasm_module from "../../dist/pkg/perspective-js.js";
 import wasm_binary from "../../dist/pkg/perspective-js.wasm";
 import { load_wasm_stage_0 } from "@finos/perspective/src/ts/decompress.ts";
 
-const module = await load_wasm_stage_0(wasm_binary as unknown as ArrayBuffer);
-await wasm_module.default(module);
-await wasm_module.init();
+import type * as psp from "../../dist/pkg/perspective-js.d.ts";
+
+type WasmElement = {
+    __wasm_module__: Promise<typeof psp>;
+};
+
+export async function compile_perspective() {
+    let elem = customElements.get(
+        "perspective-viewer"
+    ) as unknown as WasmElement;
+
+    if (!elem) {
+        console.warn(
+            "No `<perspective-viewer>` Custom Element found, using inline `Client`."
+        );
+
+        const module = await load_wasm_stage_0(
+            wasm_binary as unknown as ArrayBuffer
+        );
+
+        await wasm_module.default(module);
+        await wasm_module.init();
+        return wasm_module;
+    }
+
+    return elem.__wasm_module__;
+}
 
 export async function websocket(url: string | URL) {
-    return await api.websocket(Promise.resolve(wasm_module), url);
+    const wasm_module = compile_perspective();
+    return await api.websocket(wasm_module, url);
 }
 
 export async function worker() {
-    return await api.worker.call(undefined, Promise.resolve(wasm_module));
+    const wasm_module = compile_perspective();
+    return await api.worker(wasm_module);
 }
 
 export default { websocket, worker };
