928345f22272161386c6e35d8cebbf59ba3a9b86
Tarek Quao <tarek.quao@turntabl.io>
2025-01-14 14:32:54 +0000

    fixed formatting with prettier
    Signed-off-by: Tarek Quao <tarek.quao@turntabl.io>

--- packages/perspective-viewer-datagrid/test/js/row_header.spec.js
+++ packages/perspective-viewer-datagrid/test/js/row_header.spec.js
@@ -1,5 +1,16 @@
-import { test, expect } from "@finos/perspective-test";
+// ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+// ┃ ██████ ██████ ██████       █      █      █      █      █ █▄  ▀███ █       ┃
+// ┃ ▄▄▄▄▄█ █▄▄▄▄▄ ▄▄▄▄▄█  ▀▀▀▀▀█▀▀▀▀▀ █ ▀▀▀▀▀█ ████████▌▐███ ███▄  ▀█ █ ▀▀▀▀▀ ┃
+// ┃ █▀▀▀▀▀ █▀▀▀▀▀ █▀██▀▀ ▄▄▄▄▄ █ ▄▄▄▄▄█ ▄▄▄▄▄█ ████████▌▐███ █████▄   █ ▄▄▄▄▄ ┃
+// ┃ █      ██████ █  ▀█▄       █ ██████      █      ███▌▐███ ███████▄ █       ┃
+// ┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫
+// ┃ Copyright (c) 2017, the Perspective Authors.                              ┃
+// ┃ ╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌ ┃
+// ┃ This file is part of the Perspective library, distributed under the terms ┃
+// ┃ of the [Apache License 2.0](https://www.apache.org/licenses/LICENSE-2.0). ┃
+// ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 
+import { test, expect } from "@finos/perspective-test";
 
 test.describe("Datagrid Test for row header", () => {
     test.beforeEach(async ({ page }) => {
@@ -9,10 +20,11 @@ test.describe("Datagrid Test for row header", () => {
                 await new Promise((x) => setTimeout(x, 10));
             }
         });
+    });
 
-    })
-
-    test("verify last row tree label state after total collapse/expand", async ({ page }) => {
+    test("verify last row tree label state after total collapse/expand", async ({
+        page,
+    }) => {
         await page.evaluate(async () => {
             await document.querySelector("perspective-viewer").restore({
                 plugin: "Datagrid",
@@ -30,5 +42,5 @@ test.describe("Datagrid Test for row header", () => {
 
         await expect(westHeader).toHaveClass(/psp-tree-label-expand/);
         await expect(westHeader).not.toHaveClass(/psp-tree-label-collapse/);
-    })
-})
+    });
+});
