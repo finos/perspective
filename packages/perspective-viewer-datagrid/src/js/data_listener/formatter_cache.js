e73d5a58960a3b58886cef42a9276392c485d351
Andrew Stein <steinlink@gmail.com>
2025-01-25 16:39:27 -0500

    Fix date formatting for data grid
    Signed-off-by: Andrew Stein <steinlink@gmail.com>

--- packages/perspective-viewer-datagrid/src/js/data_listener/formatter_cache.js
+++ packages/perspective-viewer-datagrid/src/js/data_listener/formatter_cache.js
@@ -55,103 +55,104 @@ export class FormatterCache {
             dateStyle: "short",
             timeStyle: "medium",
         };
-        if (type === "datetime") {
-            if (plugin.date_format?.format !== "custom") {
-                const options = {
-                    ...type_config,
-                    timeZone: plugin.date_format?.timeZone,
-                    dateStyle: plugin.date_format?.dateStyle,
-                    timeStyle: plugin.date_format?.timeStyle,
-                };
-                if (options.dateStyle === "disabled") {
-                    options.dateStyle = undefined;
-                } else if (options.dateStyle === undefined) {
-                    options.dateStyle = "short";
-                }
 
-                if (options.timeStyle === "disabled") {
-                    options.timeStyle = undefined;
-                } else if (options.timeStyle === undefined) {
-                    options.timeStyle = "medium";
-                }
-
-                return new Intl.DateTimeFormat(navigator.languages, options);
-            } else {
-                const options = {
-                    // ...type_config.format,
-                    timeZone: plugin.date_format?.timeZone,
-                    second: plugin.date_format?.second,
-                    minute: plugin.date_format?.minute,
-                    hour: plugin.date_format?.hour,
-                    day: plugin.date_format?.day,
-                    weekday: plugin.date_format?.weekday,
-                    month: plugin.date_format?.month,
-                    year: plugin.date_format?.year,
-                    hour12: plugin.date_format?.hour12,
-                    fractionalSecondDigits:
-                        plugin.date_format?.fractionalSecondDigits,
-                };
-
-                if (options.year === "disabled") {
-                    options.year = undefined;
-                } else if (options.year === undefined) {
-                    options.year = "2-digit";
-                }
-
-                if (options.month === "disabled") {
-                    options.month = undefined;
-                } else if (options.month === undefined) {
-                    options.month = "numeric";
-                }
-
-                if (options.day === "disabled") {
-                    options.day = undefined;
-                } else if (options.day === undefined) {
-                    options.day = "numeric";
-                }
-
-                if (options.weekday === "disabled") {
-                    options.weekday = undefined;
-                }
-
-                if (options.hour === "disabled") {
-                    options.hour = undefined;
-                } else if (options.hour === undefined) {
-                    options.hour = "numeric";
-                }
-
-                if (options.minute === "disabled") {
-                    options.minute = undefined;
-                } else if (options.minute === undefined) {
-                    options.minute = "numeric";
-                }
-
-                if (options.second === "disabled") {
-                    options.second = undefined;
-                } else if (options.second === undefined) {
-                    options.second = "numeric";
-                }
-
-                if (options.hour12 === undefined) {
-                    options.hour12 = true;
-                }
-
-                return new Intl.DateTimeFormat(navigator.languages, options);
-            }
-        } else {
+        if (plugin.date_format?.format !== "custom") {
             const options = {
-                ...type_config.format,
+                ...type_config,
+                timeZone: plugin.date_format?.timeZone,
                 dateStyle: plugin.date_format?.dateStyle,
+                timeStyle: plugin.date_format?.timeStyle,
             };
-
             if (options.dateStyle === "disabled") {
                 options.dateStyle = undefined;
             } else if (options.dateStyle === undefined) {
                 options.dateStyle = "short";
             }
 
+            if (options.timeStyle === "disabled") {
+                options.timeStyle = undefined;
+            } else if (options.timeStyle === undefined) {
+                options.timeStyle = "medium";
+            }
+
+            return new Intl.DateTimeFormat(navigator.languages, options);
+        } else {
+            const options = {
+                // ...type_config.format,
+                timeZone: plugin.date_format?.timeZone,
+                second: plugin.date_format?.second,
+                minute: plugin.date_format?.minute,
+                hour: plugin.date_format?.hour,
+                day: plugin.date_format?.day,
+                weekday: plugin.date_format?.weekday,
+                month: plugin.date_format?.month,
+                year: plugin.date_format?.year,
+                hour12: plugin.date_format?.hour12,
+                fractionalSecondDigits:
+                    plugin.date_format?.fractionalSecondDigits,
+            };
+
+            if (options.year === "disabled") {
+                options.year = undefined;
+            } else if (options.year === undefined) {
+                options.year = "2-digit";
+            }
+
+            if (options.month === "disabled") {
+                options.month = undefined;
+            } else if (options.month === undefined) {
+                options.month = "numeric";
+            }
+
+            if (options.day === "disabled") {
+                options.day = undefined;
+            } else if (options.day === undefined) {
+                options.day = "numeric";
+            }
+
+            if (options.weekday === "disabled") {
+                options.weekday = undefined;
+            }
+
+            if (options.hour === "disabled") {
+                options.hour = undefined;
+            } else if (options.hour === undefined) {
+                options.hour = "numeric";
+            }
+
+            if (options.minute === "disabled") {
+                options.minute = undefined;
+            } else if (options.minute === undefined) {
+                options.minute = "numeric";
+            }
+
+            if (options.second === "disabled") {
+                options.second = undefined;
+            } else if (options.second === undefined) {
+                options.second = "numeric";
+            }
+
+            if (options.hour12 === undefined) {
+                options.hour12 = true;
+            }
+
             return new Intl.DateTimeFormat(navigator.languages, options);
         }
+    }
+
+    create_date_formatter(type, plugin) {
+        const options = {
+            dateStyle: plugin.date_format?.dateStyle,
+            timeZone: "utc",
+        };
+
+        if (options.dateStyle === "disabled") {
+            options.dateStyle = undefined;
+        } else if (options.dateStyle === undefined) {
+            options.dateStyle = "short";
+        }
+
+        return new Intl.DateTimeFormat(navigator.languages, options);
     }
 
     create_number_formatter(type, plugin) {
@@ -175,7 +176,12 @@ export class FormatterCache {
         ].join("-");
 
         if (!this._formatters.has(formatter_key)) {
-            if (type === "date" || type === "datetime") {
+            if (type === "date") {
+                this._formatters.set(
+                    formatter_key,
+                    this.create_date_formatter(type, plugin)
+                );
+            } else if (type === "datetime") {
                 this._formatters.set(
                     formatter_key,
                     this.create_datetime_formatter(type, plugin)
