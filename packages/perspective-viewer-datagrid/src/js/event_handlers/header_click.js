c81458c62617b4d36c8a7593e94551d08ac91aa8
Andrew Stein <steinlink@gmail.com>
2025-07-01 21:41:24 -0400

    Fix global filter regression
    Signed-off-by: Andrew Stein <steinlink@gmail.com>

--- packages/perspective-viewer-datagrid/src/js/event_handlers/header_click.js
+++ packages/perspective-viewer-datagrid/src/js/event_handlers/header_click.js
@@ -31,7 +31,6 @@ export async function mousedown_listener(regularTable, viewer, event) {
     }
 
     event.preventDefault();
-    event.stopImmediatePropagation();
     if (target.classList.contains("psp-tree-label")) {
         expandCollapseHandler.call(this, regularTable, event);
         return;
