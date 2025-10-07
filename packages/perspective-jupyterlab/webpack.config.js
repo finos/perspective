5e219b1e992c94d3746e3e7e070268c3b91e3c65
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-10-07 17:29:38 -0400

    Naively try updating webpack
    Signed-off-by: Tim Paine <3105306+timkpaine@users.noreply.github.com>

--- packages/perspective-jupyterlab/webpack.config.js
+++ packages/perspective-jupyterlab/webpack.config.js
@@ -11,6 +11,8 @@
 // ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 
 const CopyPlugin = require("copy-webpack-plugin");
+const { ModuleFederationPlugin } = webpack.container;
+const packageData = require("./package.json");
 
 module.exports = {
     experiments: {
@@ -19,6 +21,19 @@ module.exports = {
     plugins: [
         new CopyPlugin({
             patterns: [{ from: "./install.json", to: "../install.json" }],
+        }),
+        new ModuleFederationPlugin({
+            library: {
+                type: "var",
+                name: ["PERSPECTIVE"],
+            },
+            name: "PERSPECTIVE",
+            shared: packageData.jupyterlab.sharedPackages
+                .filter((pkg) => pkg.startsWith("@finos/perspective"))
+                .reduce((obj, pkg) => {
+                    obj[pkg] = { singleton: true };
+                    return obj;
+                }, {}),
         }),
     ],
 };
